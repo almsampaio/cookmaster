107a249b0ccfd2edd236a3c2de69f34a8e11b564
Felipe Flores <felipefloresvendas@gmail.com>
2021-09-21 15:42:39 -0300

    fix function controller call name

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -3,8 +3,8 @@ const userService = require('../services/user');
 const addProduct = async (req, res) => {
   const { email, pass, name, role } = req.body;
   const user = { email, pass, name, role };
-  const newProduct = await userService.addProduct(user);
-  
+  const newProduct = await userService.addUser(user);
+
   if (!newProduct.error) return res.status(201).json(newProduct);
   return res.status(newProduct.error).json(newProduct);
 };
