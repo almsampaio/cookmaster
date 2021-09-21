293e1d5792b4499368d8b570dccb0c5977f39a38
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-09-21 20:22:39 -0300

    feat: implementation service for Login function findByCredentials

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,5 +1,8 @@
+const jwt = require('jsonwebtoken');
 const UserModel = require('../models/UserModel');
 const userSchema = require('../schemas/UserSchema');
+
+const SECRET = 'superSecret';
 
 const create = async (user) => {
   const { error } = userSchema.userCreateValidate(user);
@@ -15,6 +18,39 @@ const create = async (user) => {
   return userCreated;
 };
 
+const validationCredentials = (email, password) => {
+  if (!email || !password) {
+    return (
+      { status: 401, message: 'All fields must be filled' }
+    );
+  }
+  return {};
+};
+
+const findByCredentials = async (email, password) => {
+  const validationEmailAndPass = validationCredentials(email, password);
+
+  if (validationEmailAndPass.status) return validationEmailAndPass;
+
+  const userSearch = await UserModel.findByEmail(email);
+
+  if (!userSearch || userSearch.email !== email || userSearch.password !== password) {
+    return (
+      { status: 401, message: 'Incorrect username or password' }
+    ); 
+  }
+
+  const { password: _, name: __, ...userPayload } = userSearch;
+
+  const token = jwt.sign(userPayload, SECRET, {
+    algorithm: 'HS256',
+    expiresIn: '15d',
+  });
+
+  return ({ status: 200, token });
+};
+
 module.exports = {
   create,
+  findByCredentials,
 };
