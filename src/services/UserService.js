be7cac4203da1167cab01f90a301e853435986a9
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-09-21 16:45:02 -0300

    fix: implementation service for UserService function create

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -4,13 +4,14 @@ const userSchema = require('../schemas/UserSchema');
 const create = async (user) => {
   const { error } = userSchema.userCreateValidate(user);
 
-  if (error) return { message: 'Invalid entries. Try again' };
+  if (error) return { message: 'Invalid entries. Try again.', code: 400 };
 
   const { message } = await userSchema.emailIsUnique(user.email);
 
-  if (message) return { message };
+  if (message) return { message, code: 409 };
 
   const userCreated = await UserModel.create(user);
+
   return userCreated;
 };
 
