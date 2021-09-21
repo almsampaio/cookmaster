d2c8cfdc029e99d3746600f76fe3f04018da6c41
RenzoSev <sevilharenzo@gmail.com>
2021-09-21 19:34:05 -0300

    feat: joi and env config

--- src/api/server.js
+++ src/api/server.js
@@ -1,5 +1,6 @@
 const app = require('./app');
+require('dotenv').config();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`conectado na porta ${PORT}`));
