c706ad1a5a5f12fadd04e58605dd1112a279bbab
Orlando Flores <orlando_flores@id.uff.br>
2021-09-21 19:13:47 -0300

    feat: finaliza rota de criacao de receita

--- src/routes/recipes.js
+++ src/routes/recipes.js
@@ -1,7 +1,10 @@
 const express = require('express');
 
 const recipesRouter = express.Router();
+const auth = require('../middlewares/auth');
+const { validateRecipe } = require('../middlewares/recipes');
+const { createRecipe } = require('../controllers/recipes');
 
-recipesRouter.post('/');
+recipesRouter.post('/', auth, validateRecipe, createRecipe);
 
 module.exports = recipesRouter;
