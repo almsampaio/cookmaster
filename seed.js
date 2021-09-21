190e3ee46fb286c3d553c3e0e5f481e772750777
AndreBarroso <andre_barroso17@hotmail.com>
2021-09-21 15:12:29 -0300

    feat: iniciando projeto

--- seed.js
+++ seed.js
@@ -1,1 +1,5 @@
 // colocar query do MongoDB
+// db.users.insertOne({ "name" : "Erick Jacquin", "email" : "erickjacquin@gmail.com", "password" : "12345678", "role" : "user" });
+
+// db.users.insertOne("name" : "Receita do Jacquin", "ingredients" : "Frango", "preparation" : "10 minutos no forno" });
+
--- src/api/app.js
+++ src/api/app.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
 
 // Não remover esse end-point, ele é necessário para o avaliador
-app.get('/', (request, response) => {
+app.get('/', (_request, response) => {
   response.send();
 });
 // Não remover esse end-point, ele é necessário para o avaliador
+
+app.get('/ping', (req, res) => {
+  res.status(200).json({ message: '' });
+});
+
+// app.use('/users', user);
+// app.use('/login', login);
+// app.use('/recipes', recipes);
+// app.use('/images', express.static('src/uploads/'));
 
 module.exports = app;
