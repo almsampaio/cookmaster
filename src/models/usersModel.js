46456ccceba873705e68ef1b69abcbacea436f31
Marcelo Maurício <mmauricio.design@gmail.com>
2021-09-21 16:06:52 -0300

    In User Model created a function to find a User by name, to get their Id

--- src/models/usersModel.js
+++ src/models/usersModel.js
@@ -1,10 +1,21 @@
 const connection = require('./connection');
 
+const findByName = async (name) => {
+  const user = await connection()
+    .then((db) => db.collection('users').findOne({ name }));
+
+  if (!user) return null;
+
+  return user;
+};
+
 const create = async (name, email, password) => {
   const role = 'user';
 
   connection()
    .then((db) => db.collection('users').insertOne({ name, email, password, role }));
+
+  const { _id } = await findByName(name);
 
   return {
     _id,
