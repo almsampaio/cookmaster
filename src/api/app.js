0170174372a4ab771445468b09b6d186069ab49f
Felipe Flores <felipefloresvendas@gmail.com>
2021-09-21 14:42:23 -0300

    feat: added model connection and user

--- src/api/app.js
+++ src/api/app.js
@@ -1,11 +1,21 @@
 const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.json());
+
+app.use('/images', express.static(path.join(__dirname, '..', 'uploads')));
 
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
   response.send();
 });
 // Não remover esse end-point, ele é necessário para o avaliador
+
+app.get('/users', (req, res) => {
+  res.send();
+});
 
 module.exports = app;
