102148a6f0cc2138a84ccab852d5b4c4e1d7ea43
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-09-21 16:46:18 -0300

    feat: endpoint create for users

--- src/api/app.js
+++ src/api/app.js
@@ -1,12 +1,18 @@
 const express = require('express');
 require('dotenv').config();
+const bodyParser = require('body-parser');
+
+const UserController = require('../controllers/UserController');
 
 const app = express();
+app.use(bodyParser.json());
 
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
   response.send();
 });
+
+app.post('/users/', UserController.create);
 // Não remover esse end-point, ele é necessário para o avaliador
 
 module.exports = app;
