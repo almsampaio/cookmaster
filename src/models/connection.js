fb94d31d5f907d5df41d1276ab376e4e8a12a9e9
RenzoSev <sevilharenzo@gmail.com>
2021-09-21 19:54:55 -0300

    fix: add mongodb connection to repo

--- src/models/connection.js
+++ src/models/connection.js
@@ -1,11 +1,12 @@
 const { MongoClient } = require('mongodb');
+require('dotenv').config();
 
 const OPTIONS = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 };
 
-const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster';
+const MONGO_DB_URL = process.env.MONGO_DB_URL || 'mongodb://mongodb:27017/Cookmaster';
 
 const DB_NAME = 'Cookmaster';
 
