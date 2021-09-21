cefdaaab29f6dcfe0aab8c07a198cef35043f8a2
ScriptCamilo <script.camilo@gmail.com>
2021-09-21 19:10:17 -0300

    feat: usersService validation in use

--- src/services/usersService.js
+++ src/services/usersService.js
@@ -1,6 +1,10 @@
 const usersModel = require('../models/usersModel');
+const validations = require('./validations');
 
 async function register(user) {
+  validations.isNameValid(user.name);
+  await validations.isEmailValid(user.email, usersModel.getByEmail);
+
   const newUserId = await usersModel.register(user);
 
   return newUserId;
