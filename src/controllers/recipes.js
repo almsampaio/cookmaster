a156b3319dc9202a97cb19ea183a864a43143009
Orlando Flores <orlando_flores@id.uff.br>
2021-09-21 19:39:41 -0300

    fix: desestruturação errada do id do usuario

--- src/controllers/recipes.js
+++ src/controllers/recipes.js
@@ -2,9 +2,9 @@ const service = require('../services/recipes');
 
 const createRecipe = async (req, res, _next) => {
   const { name, ingredients, preparation } = req.body;
-  const { userId } = req.user;
+  const { _id } = req.user;
 
-  const response = await service.createRecipe({ name, ingredients, preparation, userId });
+  const response = await service.createRecipe({ name, ingredients, preparation, userId: _id });
   res.status(201).json(response);
 };
 
