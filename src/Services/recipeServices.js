e69e76f4d76b65605ae7ddccf60cb0be8a89918f
lucas-paz <lucas.hpaz@outlook.com>
2021-09-21 19:12:50 -0300

    cria service de adicionar receita

--- src/Services/recipeServices.js
+++ src/Services/recipeServices.js
@@ -1,6 +1,21 @@
+const jwt = require('jsonwebtoken');
 const Recipes = require('../Models/recipesModel');
+const User = require('../Models/usersModel');
+const { builtError } = require('./usersServices');
 
-const create = async (payload) => {};
+const secret = 'Apenas_para_fins_didaticos_:)';
+
+const create = async (payload, token) => {
+  try {
+    const { data: { email } } = jwt.verify(token, secret);
+    const { _id: userId } = await User.findByEmail(email);
+    if (!userId) return builtError(401, 'jwt malformed');
+    const recipeId = await Recipes.create({ userId, ...payload });
+    return { recipe: { recipeId, userId, ...payload } };  
+  } catch ({ message }) {
+    return builtError(401, message);
+  }
+};
 
 module.exports = {
   create,
