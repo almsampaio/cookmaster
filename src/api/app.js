70bb37804d8158ef90624e3e5ed8d4dd504ee063
Guilherme Dugaich de Oliveira <guidugaich.dev@gmail.com>
2021-09-21 20:07:06 -0300

    body parser and userRouter

--- src/api/app.js
+++ src/api/app.js
@@ -7,5 +7,12 @@ app.get('/', (request, response) => {
   response.send();
 });
 // Não remover esse end-point, ele é necessário para o avaliador
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+const userRouter = require('../routers/userRouter');
+
+app.use('/users', userRouter);
 
 module.exports = app;
