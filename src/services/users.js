f2cbbe534a0c7dce0afd196c360a741cbd791622
Orlando Flores <orlando_flores@id.uff.br>
2021-09-21 17:09:18 -0300

    fix: objeto de erro

--- src/services/users.js
+++ src/services/users.js
@@ -3,7 +3,7 @@ const model = require('../models/users');
 const createUser = async ({ name, email, password }) => {
   const alreadyExists = await model.findUserByemail(email);
 
-  if (alreadyExists) return { code: 'invalid_data', message: 'Email already registered' };
+  if (alreadyExists) return { error: { message: 'Email already registered' } };
 
   const user = await model.createUser({ name, email, password });
   return user;
