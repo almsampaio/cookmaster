9d302b7784727546c3488b30602c046e0ed570df
RenzoSev <sevilharenzo@gmail.com>
2021-09-21 19:25:08 -0300

    feat: add api

--- src/api/app.js
+++ src/api/app.js
@@ -1,11 +1,33 @@
+const path = require('path');
+
 const express = require('express');
+const errorMiddleware = require('../middlewares/error');
 
 const app = express();
+
+const users = require('../routes/users');
+const login = require('../routes/login');
+const recipes = require('../routes/recipes');
+const images = require('../routes/images');
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, '../uploads')));
 
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
   response.send();
 });
 // Não remover esse end-point, ele é necessário para o avaliador
+
+app.use('/users', users);
+
+app.use('/login', login);
+
+app.use('/recipes', recipes);
+
+app.use('/images', images);
+
+app.use(errorMiddleware);
 
 module.exports = app;
