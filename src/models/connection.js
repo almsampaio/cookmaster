0af0bf7eac4a30b376397cd99692e5fb7fc12cac
Guilherme Dugaich de Oliveira <guidugaich.dev@gmail.com>
2021-09-21 20:07:22 -0300

    connection to DB

---
+++ src/models/connection.js
@@ -0,0 +1,21 @@
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+
+const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://mongodb:27017';
+
+let schema = null;
+
+async function getConnection() {
+  if (schema) return Promise.resolve(schema);
+  return MongoClient.connect(
+    MONGODB_URL,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+  )
+    .then((conn) => {
+      schema = conn.db('Cookmaster');
+      return schema;
+    })
+    .catch((err) => console.error(err));
+}
+
+module.exports = { getConnection };
