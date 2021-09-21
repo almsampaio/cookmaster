5170560369b249a0394f9a2b7e391d119a50cb6c
Orlando Flores <orlando_flores@id.uff.br>
2021-09-21 15:22:02 -0300

    feat: cria a conexao com o banco

---
+++ src/models/connection.js
@@ -0,0 +1,26 @@
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+
+const MONGO_DB_URL = process.env.MONGO_DB_URL || 'mongodb://mongodb:27017/Cookmaster';
+const DB_NAME = 'Cookmaster';
+
+let schema = null;
+
+async function getConnection() {
+  if (schema) return Promise.resolve(schema);
+  return MongoClient
+    .connect(MONGO_DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then((conn) => conn.db(DB_NAME))
+    .then((dbSchema) => {
+      schema = dbSchema;
+      return schema;
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
+
+module.exports = { getConnection };
