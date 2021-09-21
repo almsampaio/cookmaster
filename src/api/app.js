acbbc65ae29967cb9207b7ef1643ba425a700221
Lucas Pedroso <lepsk8@gmail.com>
2021-09-21 10:39:09 -0300

    feat: add route /users

--- src/api/app.js
+++ src/api/app.js
@@ -2,10 +2,14 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
   response.send();
 });
 // Não remover esse end-point, ele é necessário para o avaliador
+
+require('./controllers/users')(app);
 
 module.exports = app;
