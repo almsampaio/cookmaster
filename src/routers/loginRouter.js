93b8661a3d56ccffc2b767238c452b4db8be4f9e
Flavio Franco <flavio.win@gmail.com>
2021-09-21 20:24:02 -0300

    consertando module exports

--- src/routers/loginRouter.js
+++ src/routers/loginRouter.js
@@ -2,12 +2,8 @@ const express = require('express');
 const rescue = require('express-rescue');
 const controllers = require('../controllers');
 
-console.log(controllers);
-
 const loginRouter = express.Router();
 
 loginRouter.post('/', rescue(controllers.login));
 
-module.exports = {
-  loginRouter,
- };
+module.exports = loginRouter;
