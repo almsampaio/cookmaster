d91dd028544a2c5d1a5087a9b0fff45f368ea801
RenzoSev <sevilharenzo@gmail.com>
2021-09-21 19:26:48 -0300

    feat: add multers

---
+++ src/multers/recipes/image.js
@@ -0,0 +1,15 @@
+const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, '../../uploads'));
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${req.params.id}.jpeg`);
+  },
+});
+
+const upload = multer({ storage });
+
+module.exports = upload;
