2a4f703e3d042a443a3054f3aa95901af4c4322a
maricegalla <maricegalla@gmail.com>
2021-09-20 21:29:36 -0300

    post users route creation

--- src/api/app.js
+++ src/api/app.js
@@ -4,10 +4,14 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 
+const usersController = require('../controllers/usersController');
+
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
   response.send();
 });
 // Não remover esse end-point, ele é necessário para o avaliador
+
+app.post('/users', usersController.createUser);
 
 module.exports = app;
