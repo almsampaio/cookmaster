aaecd5895edaa8f96a096346795a84b199447759
lucas-paz <lucas.hpaz@outlook.com>
2021-09-21 17:33:02 -0300

    req2

--- src/Controllers/users.js
+++ src/Controllers/users.js
@@ -4,6 +4,7 @@ const createUser = async (req, res, next) => {
   const { name, email, password, role } = req.body;
 
   const newUser = await userService.registerUser({ name, email, password, role: role || 'user' });
+  console.log(newUser);
   if (newUser.message) return next(newUser);
   res.status(201).json(newUser);
 };
