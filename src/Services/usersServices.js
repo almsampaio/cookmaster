70709f5cad36cfff2d4dbbeed20e58d8bb0b206e
lucas-paz <lucas.hpaz@outlook.com>
2021-09-21 15:47:05 -0300

    adiciona tratamento de erro ao service

--- src/Services/usersServices.js
+++ src/Services/usersServices.js
@@ -1,7 +1,16 @@
+const { ObjectID } = require('bson');
 const userModels = require('../Models/usersModel');
 
-const registerUser = async (userObj) => userModels.registerUser(userObj);
+const builtError = (code, message) => ({ code, message });
+
+const registerUser = async (userObj) => {
+  const result = await userModels.registerUser(userObj);
+  const { _id: id } = result;
+  if (!ObjectID.isValid(id)) return builtError(500, 'Internal error');
+  return { user: result };
+};
 
 module.exports = {
   registerUser,
+  builtError,
 };
