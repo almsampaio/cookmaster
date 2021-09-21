6f2357597b0f56f53b3599e760d930b1a345d61a
Orlando Flores <orlando_flores@id.uff.br>
2021-09-21 18:25:30 -0300

    feat: cria rota /post para o login

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,7 +1,9 @@
 const express = require('express');
 
 const loginRouter = express.Router();
+const { login } = require('../controllers/login');
+const { loginValidation } = require('../middlewares/login');
 
-loginRouter.post('/');
+loginRouter.post('/', loginValidation, login);
 
 module.exports = loginRouter;
