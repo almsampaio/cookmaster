06efc56ccf93052328af9d2f99ce059bedf578e9
Orlando Flores <orlando_flores@id.uff.br>
2021-09-21 18:19:24 -0300

    feat: cria função que verifica se o login é correto

--- src/services/users.js
+++ src/services/users.js
@@ -1,4 +1,7 @@
+const jwt = require('jsonwebtoken');
 const model = require('../models/users');
+
+const SECRET = 'secret';
 
 const createUser = async ({ name, email, password }) => {
   const alreadyExists = await model.findUserByEmail(email);
@@ -9,6 +12,21 @@ const createUser = async ({ name, email, password }) => {
   return user;
 };
 
+const findUserByCredentials = async ({ email, password }) => {
+  const user = await model.findUserByEmail(email);
+
+  if (!user || user.password !== password) {
+    return { error: { code: 401, message: 'Incorrect username or password' } };
+  }
+
+  const { password: _, ...userInfo } = user;
+
+  const token = jwt.sign(userInfo, SECRET, { expiresIn: '15m' });
+
+  return token;
+};
+
 module.exports = {
   createUser,
+  findUserByCredentials,
 };
