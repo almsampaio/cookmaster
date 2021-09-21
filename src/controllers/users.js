295d7e027b73532039322bf5ab5afcf50a1b6a86
Orlando Flores <orlando_flores@id.uff.br>
2021-09-21 17:09:31 -0300

    feat: cria controller de cadastro de usuario

---
+++ src/controllers/users.js
@@ -0,0 +1,15 @@
+const service = require('../services/users');
+
+const createUser = async (req, res, next) => {
+  const { name, email, password } = req.body;
+
+  const response = await service.createUser({ name, email, password });
+
+  if (response.error) next(response);
+
+  return res.status(201).json(response);
+};
+
+module.exports = {
+  createUser,
+};
