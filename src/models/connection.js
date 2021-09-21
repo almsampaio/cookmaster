b4aa671ec5384ed7781606804034828aada1c224
rogerio-lambert <rogerio.cl@gmail.com>
2021-09-21 19:28:42 -0300

    adiconando variavel de ambiente para direcionar a url do mongo

--- src/models/connection.js
+++ src/models/connection.js
@@ -1,9 +1,9 @@
 const { MongoClient } = require('mongodb');
 require('dotenv').config();
 
-// const MONGO_DB_URL = process.env.MONGO_DB_URL || 'mongodb://mongodb:27017/StoreManager';
+const MONGO_DB_URL = process.env.MONGO_DB_URL || 'mongodb://mongodb:27017/StoreManager';
 
-const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
+// const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
 
 // const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager'
 
