3e233a055ca64e5a1f29912dcdfa9b58496021c1
lucas-paz <lucas.hpaz@outlook.com>
2021-09-21 16:08:13 -0300

    cria modelo de encontrar por email

--- src/Models/usersModel.js
+++ src/Models/usersModel.js
@@ -1,10 +1,22 @@
 const connect = require('./connection');
 
-const registerUser = async (userObj) =>
-  connect().then((db) =>
+const registerUser = async (userObj) => {
+  const userId = await connect().then((db) =>
     db.collection('users').insertOne(userObj))
-    .then((result) => result);
+    .then((result) => result.insertedId);
+
+  return { _id: userId, ...userObj };
+};
+
+const findByEmail = async (email) => {
+  const usersCollection = await connect()
+    .then((db) => db.collection('users'));
+
+  const user = await usersCollection.findOne({ email });
+  return user;
+};
 
 module.exports = {
   registerUser,
+  findByEmail,
 };
