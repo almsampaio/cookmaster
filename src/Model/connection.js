388dba45518e3944f773e92877fd9581db603148
Maria Baeta <m.martinsdg@gmail.com>
2021-09-22 18:42:18 -0300

    fix: :bug: Refatorando conexão com banco

--- src/Model/connection.js
+++ src/Model/connection.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const { MongoClient } = require('mongodb');
 
 const OPTIONS = {
@@ -5,7 +6,7 @@ const OPTIONS = {
   useUnifiedTopology: true,
 };
 
-const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster' || 'mongodb://mongodb:27017/Cookmaster';
+const MONGO_DB_URL = process.env.MONGO_DB_URL || 'mongodb://mongodb:27017/Cookmaster';
 const DB_NAME = 'Cookmaster';
 let db = null;
 
