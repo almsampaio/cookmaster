04521538818c80b99fa0daba18832ac69bfaca5e
Marcus C. H. Filho <mchusemann@gmail.com>
2021-09-22 15:55:11 -0300

    feat: cria util de httpStatusCodes

---
+++ src/utils/httpStatusCodes.js
@@ -0,0 +1,14 @@
+const clientErrors = {
+  badRequest: 400,
+  unauthorized: 401,
+  notFound: 404,
+  conflict: 409,
+};
+
+const successResponses = {
+  ok: 200,
+  created: 201,
+  noContent: 204,
+};
+
+module.exports = { clientErrors, successResponses };
