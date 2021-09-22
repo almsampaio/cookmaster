b6e182944abae9b7b0f9157ed68560ba46287d7b
Marcelo Maurício <mmauricio.design@gmail.com>
2021-09-22 15:25:30 -0300

    fix: the message about erro in authorization login

--- src/api/auth/validateJWT.js
+++ src/api/auth/validateJWT.js
@@ -7,7 +7,7 @@ module.exports = async (req, res, next) => {
   const token = req.headers.authorization;
 
   if (!token) {
-    return res.status(401).json({ message: 'jwt malformed' });
+    return res.status(401).json({ message: 'missing auth token' });
   }
 
   try {
