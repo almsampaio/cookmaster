aa9cc3bcfae9d890e9119a008b017e3c095454f6
Guilherme Dugaich de Oliveira <guidugaich.dev@gmail.com>
2021-09-22 15:53:35 -0300

    user service

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,8 +1,24 @@
 const userModel = require('../models/userModel');
+const userSchema = require('../schemas/userSchema');
 
-async function addUser(user) {
-  const addedUser = await userModel.addUser(user);
-  return addedUser;
+async function emailIsUnique(email) {
+  const userByEmail = await userModel.getUserByEmail(email);
+  if (!userByEmail.length) return true;
+  return false;
+}
+
+async function addUser({ name, email, password }) {
+  const validation = userSchema.validateUser({ name, email, password });
+  
+  if (validation.message) return { code: validation.code, message: validation.message };
+  
+  const uniqueEmail = await emailIsUnique(email);
+  if (!uniqueEmail) return { code: 409, message: 'Email already registered' };
+
+  const role = 'user';
+  const addedUser = await userModel.addUser({ name, email, password, role });
+  
+  return { code: 201, user: addedUser };
 }
 
 module.exports = {
