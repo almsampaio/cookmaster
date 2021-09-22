bdbd799680d3af61a2c98e3f6ba16089d304705f
Joao Nasc <oi@joaonasc.dev>
2021-09-22 19:25:45 -0300

    feat: route to get all recipes

--- src/api/routes/recipes.js
+++ src/api/routes/recipes.js
@@ -2,6 +2,7 @@ const routes = require('express').Router();
 const recipesController = require('../controllers/recipes');
 const auth = require('../middlewares/auth');
 
+routes.get('/', recipesController.get);
 routes.post('/', auth, recipesController.create);
 
 module.exports = routes;
