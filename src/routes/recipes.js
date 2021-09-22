12af47823536f31686bb173684f3c063c6d7b2c3
Orlando Flores <orlando_flores@id.uff.br>
2021-09-22 15:43:43 -0300

    feat: cria rota especifica para buscar receita por id

--- src/routes/recipes.js
+++ src/routes/recipes.js
@@ -3,8 +3,10 @@ const express = require('express');
 const recipesRouter = express.Router();
 const auth = require('../middlewares/auth');
 const { validateRecipe, validateIfUserIsAuth } = require('../middlewares/recipes');
-const { createRecipe, getAllRecipes } = require('../controllers/recipes');
+const { createRecipe, getAllRecipes, getRecipeById } = require('../controllers/recipes');
 
 recipesRouter.post('/', auth, validateRecipe, createRecipe);
 recipesRouter.get('/', validateIfUserIsAuth, getAllRecipes);
+recipesRouter.get('/:id', getRecipeById);
+
 module.exports = recipesRouter;
