86f8a8834b3df9baf0773d093c8b4ab43fbb5453
PollyanaOliveira <pollyana.deoliveir@gmail.com>
2021-09-22 10:13:28 -0400

    feat: :sparkles: create router

--- src/api/server.js
+++ src/api/server.js
@@ -1,4 +1,11 @@
-const app = require('./app');
+const express = require('express');
+const bodyParser = require('body-parser');
+const router = require('./routers');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.use(router);
 
 const PORT = 3000;
 
