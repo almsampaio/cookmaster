5dcf9daa51ade9d5ea0c488d480ef4059e811e91
Joao Nasc <oi@joaonasc.dev>
2021-09-22 17:46:06 -0300

    refactor: move error messages to schema

--- src/api/schemas/user.js
+++ src/api/schemas/user.js
@@ -5,5 +5,8 @@ module.exports = {
     name: Joi.string().required(),
     email: Joi.string().email().required(),
     password: Joi.string().required(),
+  }).messages({ 
+    'any.required': 'Invalid entries. Try again.',
+    'string.email': 'Invalid entries. Try again.',
   }),
 };
--- src/api/services/users.js
+++ src/api/services/users.js
@@ -5,14 +5,14 @@ const CustomError = require('../../lib/CustomError');
 module.exports = {
   async create(body) {
     const userSchemaError = userCreation.validate(body).error;
-    if (userSchemaError) {
-      throw new CustomError(400, 'Invalid entries. Try again.');
-    }
+
+    if (userSchemaError) throw new CustomError(400, userSchemaError.message);
+
     const { name, email, password } = body;
+
     const userFound = await usersModel.get.byEmail(email);
-    if (userFound) {
-      throw new CustomError(409, 'Email already registered');
-    }
+    if (userFound) throw new CustomError(409, 'Email already registered');
+
     return usersModel.create({ name, email, password });
   },
 };
