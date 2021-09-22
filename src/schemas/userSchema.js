c8614728f145f359d2d3cdc060b148add00cf204
Guilherme Dugaich de Oliveira <guidugaich.dev@gmail.com>
2021-09-22 16:59:20 -0300

    moving functions do validations.js

--- src/schemas/userSchema.js
+++ src/schemas/userSchema.js
@@ -1,19 +1,7 @@
-function emailIsValid(email) {
-  if (!email) return false;
-
-  const emailRegex = /[a-z0-9]+@[a-z0-9]+(\.com)$/gi;
-  return emailRegex.test(email);
-}
-
-function nameIsValid(name) {
-  if (!name) return false;
-  return true;
-}
-
-function passwordIsValid(password) {
-  if (!password) return false;
-  return true;
-}
+const {
+  emailIsValid,
+  nameIsValid,
+  passwordIsValid } = require('../validations');
 
 function validateUser({ name, email, password }) {
   if (!nameIsValid(name) || !emailIsValid(email) || !passwordIsValid(password)) {
