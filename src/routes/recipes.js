d3df277b482ee35edd13aa7e7781f6be317235cb
Orlando Flores <orlando_flores@id.uff.br>
2021-09-22 16:45:56 -0300

    feat: adiciona a rota de atualizar receita

--- src/routes/recipes.js
+++ src/routes/recipes.js
@@ -3,10 +3,15 @@ const express = require('express');
 const recipesRouter = express.Router();
 const auth = require('../middlewares/auth');
 const { validateRecipe, validateIfUserIsAuth } = require('../middlewares/recipes');
-const { createRecipe, getAllRecipes, getRecipeById } = require('../controllers/recipes');
+const {
+  createRecipe,
+  getAllRecipes,
+  getRecipeById,
+  updatedRecipe,
+} = require('../controllers/recipes');
 
 recipesRouter.post('/', auth, validateRecipe, createRecipe);
 recipesRouter.get('/', validateIfUserIsAuth, getAllRecipes);
 recipesRouter.get('/:id', getRecipeById);
-
+recipesRouter.put('/:id', auth, updatedRecipe);
 module.exports = recipesRouter;
