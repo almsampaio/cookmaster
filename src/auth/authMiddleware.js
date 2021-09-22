41a4d722bf54ea2b01deea93b6c1f928a0873a39
Guilherme Dugaich de Oliveira <guidugaich.dev@gmail.com>
2021-09-22 19:27:30 -0300

    changing msg for missing token

--- src/auth/authMiddleware.js
+++ src/auth/authMiddleware.js
@@ -5,7 +5,7 @@ function validateJWT(req, res, next) {
   const token = req.headers.authorization;
 
   if (!token) {
-    return res.status(401).json({ message: 'jwt malformed' });
+    return res.status(401).json({ message: 'missing auth token' });
   }
 
   try {
