baef13379846f03367a5db01ded507aa8cbad9ab
Debora Passos <deborinhapassos@gmail.com>
2021-09-22 17:50:44 -0300

    Criando conections e app

--- src/api/app.js
+++ src/api/app.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
 
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
   response.send();
 });
 // Não remover esse end-point, ele é necessário para o avaliador
+
+// ...
 
 module.exports = app;
