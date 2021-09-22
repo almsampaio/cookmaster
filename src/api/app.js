a067f06033b1f26f52f31e3818c953a243459ada
Joao Nasc <oi@joaonasc.dev>
2021-09-21 23:39:24 -0300

    feat: config app

--- src/api/app.js
+++ src/api/app.js
@@ -1,6 +1,12 @@
+require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
+const usersRoutes = require('./routes/users');
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use('/users', usersRoutes);
 
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
