544a88d18dc0509c055743329b1dded06b4faf4c
Marcelo Maurício <mmauricio.design@gmail.com>
2021-09-22 16:03:46 -0300

    Created the functions about upload image to the DB

--- src/api/app.js
+++ src/api/app.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const multer = require('multer');
+const path = require('path');
 
 const Users = require('../controllers/usersController');
 const Recipes = require('../controllers/recipesController');
-// const login = require('../controllers/login');
 const validateJWT = require('./auth/validateJWT');
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '..', '/uploads')));
+
+const upload = multer({ dest: 'uploads' });
 
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
@@ -28,5 +32,6 @@ app.get('/recipes', Recipes.getAll);
 app.get('/recipes/:id', Recipes.findById);
 app.put('/recipes/:id', validateJWT, Recipes.update);
 app.delete('/recipes/:id', validateJWT, Recipes.deleteInfo);
+app.post('/recipes/:id/image/', validateJWT, upload.single('file'), Recipes.insertImage);
 
 module.exports = app;
