6f66f3f085b264ef1134933cbf86924b2bd76052
Orlando Flores <orlando_flores@id.uff.br>
2021-09-22 15:22:50 -0300

    feat: cria rota para todas as receitas

--- src/routes/recipes.js
+++ src/routes/recipes.js
@@ -2,9 +2,9 @@ const express = require('express');
 
 const recipesRouter = express.Router();
 const auth = require('../middlewares/auth');
-const { validateRecipe } = require('../middlewares/recipes');
-const { createRecipe } = require('../controllers/recipes');
+const { validateRecipe, validateIfUserIsAuth } = require('../middlewares/recipes');
+const { createRecipe, getAllRecipes } = require('../controllers/recipes');
 
 recipesRouter.post('/', auth, validateRecipe, createRecipe);
-
+recipesRouter.get('/', validateIfUserIsAuth, getAllRecipes);
 module.exports = recipesRouter;
