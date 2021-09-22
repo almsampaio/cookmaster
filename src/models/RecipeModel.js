b71f9f3f6a5764ae8e99ca5af302aa9241797639
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-09-22 15:13:25 -0300

    feat: implementation model for recipeModel function update

--- src/models/RecipeModel.js
+++ src/models/RecipeModel.js
@@ -3,6 +3,28 @@ const mongoConnection = require('./connection');
 
 const COLLECTION_NAME = 'recipes';
 
+const update = async (id, newDataRecipe) => {
+  const { name, ingredients, preparation } = newDataRecipe;
+
+  if (!ObjectId.isValid(id)) return null;
+
+  const recipeCollection = await mongoConnection.getConnection()
+  .then((db) => db.collection(COLLECTION_NAME));
+
+  const updatedRecipe = await recipeCollection
+    .findOneAndUpdate(
+      { _id: ObjectId(id) },
+      { $set: { name, ingredients, preparation } },
+      { 
+        // upsert: true,
+        // returnDocument: 'after',
+        returnOriginal: false,
+      },
+    );
+
+  return updatedRecipe.value;
+};
+
 const getById = async (id) => {
   if (!ObjectId.isValid(id)) return null;
 
@@ -30,7 +52,7 @@ const create = async (recipe, userId) => {
   .then((db) => db.collection(COLLECTION_NAME));
 
   const { insertedId: _id } = await recipeCollection.insertOne(
-    { name, ingredients, preparation },
+    { name, ingredients, preparation, userId },
   );
 
   return {
@@ -48,4 +70,5 @@ module.exports = {
   create,
   getAll,
   getById,
+  update,
 };
