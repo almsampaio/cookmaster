57a1c6aa1a933e446e2e2ff94c79543e9a670a59
PollyanaOliveira <pollyana.deoliveir@gmail.com>
2021-09-22 10:44:20 -0400

    fix: :zap: adjust connection

--- src/api/models/connection.js
+++ src/api/models/connection.js
@@ -2,7 +2,6 @@ const { MongoClient } = require('mongodb');
 require('dotenv').config();
 
 const MONGO_DB_URL = process.env.MONGO_DB_URL || 'mongodb://mongodb:27017/Cookmaster';
-
 const OPTIONS = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
