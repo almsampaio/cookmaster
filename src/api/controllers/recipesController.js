dd98986104dd0c5cd79260fbfda2b900cca4b386
Paulo Vitor Borges <pvtrybe209@gmail.com>
2021-09-22 15:20:38 -0300

    feat:requisito 3

--- src/api/controllers/recipesController.js
+++ src/api/controllers/recipesController.js
@@ -5,9 +5,10 @@ const register = async (req, res) => {
     const { user } = req;
   
     const { message, _id } = await recipesService.register(name, ingredients, preparation);
+    console.log(_id);
   
     if (message) return res.status(400).json({ message });
-    res.status(201).json({ user: { name, ingredients, preparation, userId: user, _id } });
+    res.status(201).json({ recipe: { name, ingredients, preparation, userId: user, _id } });
   };
 
   module.exports = {
