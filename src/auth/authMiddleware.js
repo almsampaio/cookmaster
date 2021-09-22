e86b726e3f2044d862a32060b8d4af306ec4f7bd
Guilherme Dugaich de Oliveira <guidugaich.dev@gmail.com>
2021-09-22 18:19:40 -0300

    auth middleware - validateJWT

---
+++ src/auth/authMiddleware.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+const JWTsecret = require('./JWTsecret');
+
+function validateJWT(req, res, next) {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).json({ message: 'jwt malformed' });
+  }
+
+  try {
+    const { email, userId } = jwt.verify(token, JWTsecret);
+    req.email = email;
+    req.userId = userId;
+  } catch (err) {
+    return res.status(401).json({ message: 'jwt malformed' });
+  }
+
+  next();
+}
+
+module.exports = {
+  validateJWT,
+};
