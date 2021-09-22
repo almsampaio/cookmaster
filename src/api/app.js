652e9eb22a84323992644c28bcb35e6c18a6abdd
Marcus C. H. Filho <mchusemann@gmail.com>
2021-09-22 15:42:14 -0300

    feat: config app.js

--- src/api/app.js
+++ src/api/app.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const usersRoutes = require('../routes/users');
+const errors = require('../middlewares/error');
+const loginRoutes = require('../routes/login');
+const recipesRoutes = require('../routes/recipes');
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use('/users', usersRoutes);
+app.use('/login', loginRoutes);
+app.use('/recipes', recipesRoutes);
+
 // Não remover esse end-point, ele é necessário para o avaliador
-app.get('/', (request, response) => {
+app.get('/', (_request, response) => {
   response.send();
 });
-// Não remover esse end-point, ele é necessário para o avaliador
+
+app.use(errors);
 
 module.exports = app;
