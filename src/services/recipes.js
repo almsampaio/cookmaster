cc81a93f808bb7f5fd5f0ce75f1c2792ea8585e8
Orlando Flores <orlando_flores@id.uff.br>
2021-09-22 15:04:14 -0300

    feat: cria service q retorna todas as receitas

--- src/services/recipes.js
+++ src/services/recipes.js
@@ -5,6 +5,12 @@ const createRecipe = async ({ name, ingredients, preparation, userId }) => {
   return recipe;
 };
 
+const getAllRecipes = async () => {
+  const recipes = await model.getAllRecipes();
+  return recipes;
+};
+
 module.exports = {
   createRecipe,
+  getAllRecipes,
 };
