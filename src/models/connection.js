3e8aa6f65636bbac35be5fc2ffe0fd14e3eb89f2
hannibal1207 <mathausannibal@gmail.com>
2021-09-22 09:22:14 -0300

    muda a url

--- src/models/connection.js
+++ src/models/connection.js
@@ -2,9 +2,9 @@
 
 const { MongoClient } = require('mongodb');
 
-const MONGO_DB_URL = 'mongodb://127.0.0.1:27017/Cookmaster';
+// const MONGO_DB_URL = 'mongodb://127.0.0.1:27017/Cookmaster';
 
-// const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
+const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
 
 const DB_NAME = 'Cookmaster';
 
