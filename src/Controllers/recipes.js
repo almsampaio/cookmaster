cc463b9088b17743acf7c4d4632940b21793e16f
lucas-paz <lucas.hpaz@outlook.com>
2021-09-22 11:28:20 -0300

    cria os controllers

--- src/Controllers/recipes.js
+++ src/Controllers/recipes.js
@@ -8,6 +8,20 @@ const createRecipe = async (req, res, next) => {
   return res.status(201).json(result);
 };
 
+const getAll = async (_req, res) => {
+  const result = await Recipes.getAll();
+  return res.status(200).json(result);
+};
+
+const getById = async (req, res, next) => {
+  const { id } = req.params;
+  const result = await Recipes.getById(id);
+  if (result.message) return next(result);
+  return res.status(200).json(result);
+};
+
 module.exports = {
   createRecipe,
+  getAll,
+  getById,
 };
