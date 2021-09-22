9b732e4961572bf5aa8023d976fd55530f582738
Joao Nasc <oi@joaonasc.dev>
2021-09-22 19:42:23 -0300

    feat: get recipe from db by id

--- src/api/models/recipes.js
+++ src/api/models/recipes.js
@@ -1,9 +1,13 @@
+const { ObjectId } = require('mongodb');
 const connect = require('./connection');
 
 module.exports = {
   async get(id) {
     const db = await connect();
+    
     if (!id) return db.collection('recipes').find({}).toArray();
+    
+    return db.collection('recipes').findOne({ _id: ObjectId(id) });
   },
 
   async create(recipe, user) {
