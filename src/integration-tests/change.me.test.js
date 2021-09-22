e6251965cdcec0e0a790e067a6cdacebc1f12788
maricegalla <maricegalla@gmail.com>
2021-09-22 15:59:17 -0300

    Requirement 11

--- src/integration-tests/change.me.test.js
+++ src/integration-tests/change.me.test.js
@@ -1,2 +1,26 @@
-describe('', () => {
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../api/app');
+
+chai.use(chaiHttp);
+const { expect } = chai;
+
+describe('POST /users', () => {
+  describe('when is created', () => {
+    let response = {};
+
+    before(async () => {
+      response = await chai.request(server)
+          .post('/users')
+          .send({
+              name: 'jane',
+              email: 'jane@gmail.com',
+              password: 'senha123'
+          });
+  });
+
+    it('returns status code 201', () => {
+      expect(response).to.have.status(201);
+    });
+  })
 });
