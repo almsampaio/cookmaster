6830d0038f9ee03a5c5d2f64704624380fe1e7dd
luanlsr <luanfswd@gmail.com>
2021-09-21 23:55:52 -0300

    feat: :white_check_mark: req 6 implemented

--- test/admin.test.js
+++ test/admin.test.js
@@ -5,7 +5,7 @@ const { MongoClient } = require('mongodb');
 const mongoDbUrl = 'mongodb://localhost:27017/Cookmaster';
 const url = 'http://localhost:3000';
 
-describe('6 - Crie uma query em mongo que insira uma pessoa usuária com permissões de admin', () => {
+describe.only('6 - Crie uma query em mongo que insira uma pessoa usuária com permissões de admin', () => {
   let connection;
   let db;
 
