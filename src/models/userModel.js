a2c53111b7ea94ec2ccc1a2c3e348e113a7aeb4f
Guilherme Dugaich de Oliveira <guidugaich.dev@gmail.com>
2021-09-22 15:53:19 -0300

    user model

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,14 +1,31 @@
 const mongoConnection = require('./connection');
 
+async function getUserByEmail(email) {
+  const db = await mongoConnection.getConnection();
+
+  const userByEmail = await db.collection('users').find(
+    { email },
+    {},
+  ).toArray();
+
+  return userByEmail;
+}
+
 async function addUser({ name, email, password, role }) {
   const db = await mongoConnection.getConnection();
-  const addedUser = db.collection('user').insertOne(
+  const { insertedId: id } = await db.collection('users').insertOne(
     { name, email, password, role },
   );
-
-  return addedUser;
+    console.log(id);
+  return {
+    name, 
+    email,
+    role,
+    _id: id,
+  };
 }
 
 module.exports = {
+  getUserByEmail,
   addUser,
 };
