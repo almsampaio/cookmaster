6b39e4932895647e3011a69128e514d7fac27b94
Orlando Flores <orlando_flores@id.uff.br>
2021-09-22 15:17:54 -0300

    feat: cria middleware q decide se o usuario esta logado ou n

--- src/middlewares/recipes.js
+++ src/middlewares/recipes.js
@@ -1,3 +1,5 @@
+const auth = require('./auth');
+
 const validateRecipe = (req, res, next) => {
   const { name, ingredients, preparation } = req.body;
   if (!name || !ingredients || !preparation) {
@@ -6,6 +8,13 @@ const validateRecipe = (req, res, next) => {
   next();
 };
 
+const validateIfUserIsAuth = (req, _res, next) => {
+  const token = req.headers.authorization;
+  if (token) return auth;
+  next();
+};
+
 module.exports = {
   validateRecipe,
+  validateIfUserIsAuth,
 };
