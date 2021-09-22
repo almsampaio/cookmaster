44d26aa78878f74acbbbd4f089f2b020f2b97fe5
Joao Nasc <oi@joaonasc.dev>
2021-09-22 20:19:15 -0300

    feat: update recipe of db

--- src/api/models/recipes.js
+++ src/api/models/recipes.js
@@ -19,4 +19,12 @@ module.exports = {
 
     return recipeCreated.ops[0];
   },
+
+  async update(id, updatesForRecipe) {
+    const db = await connect();
+    const currentRecipe = await this.get(id);
+    const recipeUpdated = { ...currentRecipe, ...updatesForRecipe };
+    await db.collection('recipes').updateOne({ _id: ObjectId(id) }, { $set: recipeUpdated });
+    return recipeUpdated;
+  },
 };
