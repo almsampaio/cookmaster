35b30059f11e964c601fac6c85e36ae59ac82014
Orlando Flores <orlando_flores@id.uff.br>
2021-09-22 16:45:29 -0300

    feat: adiciona mais uma verificacao ao auth middleware

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -6,6 +6,9 @@ const SECRET = 'secret';
 const auth = async (req, res, next) => {
   try {
     const token = req.headers.authorization;
+
+    if (!token) return res.status(401).json({ message: 'missing auth token' });
+
     const { email } = jwt.verify(token, SECRET);
 
     const user = await usersModel.findUserByEmail(email);
