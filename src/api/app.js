66cde11e26623d92300fc052d5e71310ab08b3cc
lucas-paz <lucas.hpaz@outlook.com>
2021-09-22 17:53:20 -0300

    configura o multer

--- src/api/app.js
+++ src/api/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
+const multer = require('multer');
 const routes = require('./routes');
 const { handleErrors } = require('../Middlewares/erros');
 const { validateNewUser, validateLogin, validateNewRecipe } = require('../Middlewares/usersMid');
@@ -9,6 +11,20 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, '..', 'uploads')));
+
+const storage = multer.diskStorage({
+  destination: (_req, _file, callback) => {
+    callback(null, 'uploads');
+  },
+  filename: (req, file, callback) => {
+    const { id } = req.params;
+    callback(null, `${id}.jpeg`);
+  },
+});
+
+const upload = multer({ storage });
 
 app.get('/', (request, response) => {
   response.send();
@@ -22,6 +38,7 @@ apiRoutes.get('/recipes/:id', routes.getById);
 apiRoutes.get('/recipes', routes.getAll);
 apiRoutes.put('/recipes/:id', validateJWT, validateNewRecipe, routes.update);
 apiRoutes.delete('/recipes/:id', validateJWT, routes.remove);
+apiRoutes.put('/recipes/:id/image/', validateJWT, upload.single('image'));
 
 app.use(apiRoutes);
 app.use(handleErrors);
