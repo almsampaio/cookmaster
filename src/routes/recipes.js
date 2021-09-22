94289f737bdc1f9d8af53dda5cec2c4e9dcb2d14
Orlando Flores <orlando_flores@id.uff.br>
2021-09-22 19:18:22 -0300

    feat: cria rota para adicionar imagem

--- src/routes/recipes.js
+++ src/routes/recipes.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const path = require('path');
 
 const recipesRouter = express.Router();
 const auth = require('../middlewares/auth');
+const upload = require('../middlewares/imgUpload');
 const { validateRecipe, validateIfUserIsAuth } = require('../middlewares/recipes');
 const {
   createRecipe,
@@ -9,12 +11,16 @@ const {
   getRecipeById,
   updatedRecipe,
   deleteRecipe,
+  addImage,
 } = require('../controllers/recipes');
+
+recipesRouter.use(express.static(path.join(__dirname, '..', 'uploads')));
 
 recipesRouter.post('/', auth, validateRecipe, createRecipe);
 recipesRouter.get('/', validateIfUserIsAuth, getAllRecipes);
 recipesRouter.get('/:id', getRecipeById);
 recipesRouter.put('/:id', auth, updatedRecipe);
 recipesRouter.delete('/:id', auth, deleteRecipe);
+recipesRouter.put('/:id/image', auth, upload.single('image'), addImage);
 
 module.exports = recipesRouter;
