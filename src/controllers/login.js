fb0798f6dc6407e2669c797d472032c5b0460984
Flavio Franco <flavio.win@gmail.com>
2021-09-22 10:38:04 -0300

    retirando o auth

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -2,12 +2,16 @@ const jwt = require('jsonwebtoken');
 const service = require('../services');
 
 const JWT_SECRET = '123';
+const jwtConfig = {
+  expiresIn: '1h',
+  algorithm: 'HS256',
+};
 
 module.exports = async (req, res) => {
   const { status, user: { _id: id, email, role } } = await service.login(req.body);
 
   const payload = { id, email, role };
-  const token = jwt.sign(payload, JWT_SECRET);
+  const token = jwt.sign({ data: payload }, JWT_SECRET, jwtConfig);
 
   res.status(status).json({ token });
 };
