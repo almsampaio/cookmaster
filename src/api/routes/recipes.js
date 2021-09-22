6fe9af062efc5be1f5933a1a4e544e9c896dc768
Joao Nasc <oi@joaonasc.dev>
2021-09-22 19:44:05 -0300

    feat: route to get recipe by id

--- src/api/routes/recipes.js
+++ src/api/routes/recipes.js
@@ -2,6 +2,7 @@ const routes = require('express').Router();
 const recipesController = require('../controllers/recipes');
 const auth = require('../middlewares/auth');
 
+routes.get('/:id', recipesController.get);
 routes.get('/', recipesController.get);
 routes.post('/', auth, recipesController.create);
 
