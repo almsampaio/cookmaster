c57dd0daeda449d66ad92a5abfb92ccab8f2c4e0
Joao Nasc <oi@joaonasc.dev>
2021-09-22 18:27:16 -0300

    feat: use /login route

--- src/api/app.js
+++ src/api/app.js
@@ -2,11 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const usersRoutes = require('./routes/users');
+const loginRoutes = require('./routes/login');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use('/users', usersRoutes);
+app.use('/login', loginRoutes);
 
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
