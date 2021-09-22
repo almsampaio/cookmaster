599b32d1959d9306af59fad2731047c29e0e969a
Lucas Lara <lucasml@live.com>
2021-09-22 15:12:37 -0300

    checking credentials correctly

--- middlewares/login.js
+++ middlewares/login.js
@@ -1,13 +1,19 @@
-const { StatusCodes: { UNAUTHORIZED } } = require('http-status-codes');
+const {
+  StatusCodes: { UNAUTHORIZED },
+} = require('http-status-codes');
 const Schema = require('../validations/schemas');
 const userModel = require('../models/userModel');
 
 const login = async (req, _res, next) => {
   const { error } = Schema.loginSchema.validate(req.body);
-  if (error) next({ message: 'All fields must be filled', statusCode: UNAUTHORIZED });
+    if (error) next({ message: 'All fields must be filled', statusCode: UNAUTHORIZED });
   const userExists = await userModel.findOnebyEmail(req.body.email);
-  if (!userExists) next({ message: 'Incorrect username or password', statusCode: UNAUTHORIZED });
+    if (!userExists || userExists.password !== req.body.password) {
+  next({
+        message: 'Incorrect username or password',
+        statusCode: UNAUTHORIZED,
+      }); 
+}
   next();
 };
-
 module.exports = login;
