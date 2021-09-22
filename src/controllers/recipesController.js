13d282a966c570462a9c96473ef2cd51ba95b753
Marcelo Maurício <mmauricio.design@gmail.com>
2021-09-22 08:57:31 -0300

    Created the Controller of Recipes MSC, with the create function

---
+++ src/controllers/recipesController.js
@@ -0,0 +1,21 @@
+const rescue = require('express-rescue');
+
+const service = require('../services/recipesService');
+
+const ERROR_MESSAGE = 'Invalid entries. Try again.';
+
+const create = rescue(async (req, res) => {
+  const { name, ingredients, preparation } = req.body;
+
+  if (!name || !ingredients || !preparation) {
+    return res.status(400).json({ message: ERROR_MESSAGE });
+   }
+
+   const createRecipe = await service.create(name, ingredients, preparation);
+
+   res.status(201).json(createRecipe);
+});
+
+module.exports = {
+  create,
+};
