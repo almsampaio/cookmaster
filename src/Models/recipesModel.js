d506cf4f7f98dc7f5a96c5875bd2a67e695f5f99
lucas-paz <lucas.hpaz@outlook.com>
2021-09-22 12:24:10 -0300

    cria os models de deletar e atualizar

--- src/Models/recipesModel.js
+++ src/Models/recipesModel.js
@@ -17,7 +17,29 @@ const get = async (param) => {
   return recipesCollection.findOne({ _id: ObjectID(param) });
 };
 
+const update = async (recipeObj) => {
+  const { email } = recipeObj;
+  const recipesCollection = await connect.getConnection()
+    .then((db) => db.collection('recipes'));
+
+  return recipesCollection.findOneAndUpdate(
+    { email },
+    { $set: { ...recipeObj } },
+    { returnDocument: 'after' },
+  );
+};
+
+const remove = async (recipeObj) => {
+  const { email } = recipeObj;
+  const recipesCollection = await connect.getConnection()
+    .then((db) => db.collection('recipes'));
+
+  return recipesCollection.findOneAndDelete({ email });
+};
+
 module.exports = {
   create,
   get,
+  update,
+  remove,
 };
