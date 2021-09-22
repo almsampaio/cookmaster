358d8410137e6c83a95082e73f95701562301a73
Marcelo Maurício <mmauricio.design@gmail.com>
2021-09-22 11:41:51 -0300

    Inserted findById function in Recipe Services

--- src/services/recipesService.js
+++ src/services/recipesService.js
@@ -6,7 +6,23 @@ const create = async (name, ingredients, preparation, userId) => (
 
 const getAll = async () => Recipes.getAll();
 
+const findById = async (id) => { 
+  const recipe = await Recipes.findById(id);
+
+  if (!recipe) {
+    return {
+      err: {
+        code: 'invalid_data',
+        message: 'recipe not found',
+      },
+    };
+  }
+
+  return recipe;
+};
+
 module.exports = {
   create,
   getAll,
+  findById,
 };
