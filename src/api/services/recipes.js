3deee252842e896e80532cf2ecb033cac38a8a25
Joao Nasc <oi@joaonasc.dev>
2021-09-22 19:08:36 -0300

    feat: recipes service

---
+++ src/api/services/recipes.js
@@ -0,0 +1,12 @@
+const { recipeCreation } = require('../schemas/recipes');
+const CustomError = require('../../lib/CustomError');
+const recipesModel = require('../models/recipes');
+
+module.exports = {
+  async create(recipe, user) {
+    const recipeSchemaError = recipeCreation.validate(recipe).error;
+    
+    if (recipeSchemaError) throw new CustomError(400, recipeSchemaError.message);
+    return recipesModel.create(recipe, user);
+  },
+};
