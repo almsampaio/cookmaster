6074c7f14191776f54eb885b71fdbcdbd9a0dcb1
maricegalla <maricegalla@gmail.com>
2021-09-22 16:06:02 -0300

    req 9 not pass

--- src/api/app.js
+++ src/api/app.js
@@ -15,6 +15,7 @@ const { authBasic } = require('../auth/basicAuth');
 app.get('/', (request, response) => {
   response.send();
 });
+
 // Não remover esse end-point, ele é necessário para o avaliador
 
 const storage = multer.diskStorage({
