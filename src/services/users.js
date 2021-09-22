992e17d52f21eca37c8a4b338b4b9b353958f3ad
Marcus C. H. Filho <mchusemann@gmail.com>
2021-09-22 15:54:06 -0300

    feat: cria services de usuarios

---
+++ src/services/users.js
@@ -0,0 +1,21 @@
+const usersModel = require('../models/users');
+const { clientErrors } = require('../utils/httpStatusCodes');
+
+const existsEmail = async (email) => {
+  const isRegistered = await usersModel.existsEmail(email);
+  if (isRegistered) {
+    return {
+      statusCode: clientErrors.conflict, message: 'Email already registered',
+    };
+  }
+  return isRegistered;
+};
+
+const getUserById = async (id) => usersModel.getUserById(id);
+
+const createUser = async (name, email, password) => {
+  const role = 'user';
+  const newUser = await usersModel.createUser(name, email, password, role);
+  return newUser;
+};
+module.exports = { existsEmail, createUser, getUserById };
