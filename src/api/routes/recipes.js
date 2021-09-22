344066eaed9734c79e8df9c2ac43d420c274c8bd
Joao Nasc <oi@joaonasc.dev>
2021-09-22 20:20:09 -0300

    feat: route to update recipe

--- src/api/routes/recipes.js
+++ src/api/routes/recipes.js
@@ -3,6 +3,7 @@ const recipesController = require('../controllers/recipes');
 const auth = require('../middlewares/auth');
 
 routes.get('/:id', recipesController.get);
+routes.put('/:id', auth, recipesController.update);
 routes.get('/', recipesController.get);
 routes.post('/', auth, recipesController.create);
 
