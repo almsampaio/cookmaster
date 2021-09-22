2deca34156542d9a42862b7839ca53fb49cd7354
Marcus C. H. Filho <mchusemann@gmail.com>
2021-09-22 15:55:49 -0300

    feat: cria model de conexao com o mongo

---
+++ src/models/connection.js
@@ -0,0 +1,23 @@
+const { MongoClient } = require('mongodb');
+
+const OPTIONS = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+// configuração local
+const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
+// configuração para o avaliador
+// const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
+const DB_NAME = 'Cookmaster';
+let db = null;
+
+const connection = () => (db
+  ? Promise.resolve(db)
+  : MongoClient.connect(MONGO_DB_URL, OPTIONS)
+    .then((conn) => {
+      db = conn.db(DB_NAME);
+      return db;
+    }));
+
+module.exports = connection;
