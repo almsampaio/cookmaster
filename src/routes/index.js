e9c4bbd8ed3ae9e45194a84aeac089c8c2a365e7
Rafael Medeiros <62731714+RafaelMedeirosGomes@users.noreply.github.com>
2021-09-22 12:57:24 -0300

    Small changes in routes file

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,16 +6,14 @@ const { recipesController } = require('../controllers');
 
 const routes = express.Router();
 
-routes.use('/images', express.static(path.join(__dirname, '..', 'uploads')));
-// Não remover esse end-point, ele é necessário para o avaliador
-routes.get('/', (_req, res) => {
-  res.send('Hi');
-});
-// Não remover esse end-point, ele é necessário para o avaliador
 routes.post('/users', usersController.create);
 routes.post('/login', loginController.login);
 routes.post('/recipes', recipesController.create);
 routes.get('/recipes', recipesController.readMany);
 routes.get('/recipes/:id', recipesController.readOne);
+// Não remover esse end-point, ele é necessário para o avaliador
+routes.use('/images', express.static(path.join(__dirname, '..', 'uploads')));
+// Não remover esse end-point, ele é necessário para o avaliador
+routes.get('/', (_req, res) => res.send());
 
 module.exports = routes;
