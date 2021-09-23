2f41014194992e489e40f2bb1bbaaa818c8d8c42
Tiago Santos <tiago881@gmail.com>
2021-09-23 10:52:21 -0300

    feat: requirement 1 - search by email

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,11 +1,14 @@
 const userModel = require('../models/user-model');
 
 const createUser = async (name, email, password) => {
- const creteadUser = await userModel.createUser(name, email, password);
+  const creteadUser = await userModel.createUser(name, email, password);
 
- return creteadUser;
+  return creteadUser;
 };
+
+const findEmail = async (email) => userModel.findEmail(email);
 
 module.exports = {
   createUser,
+  findEmail,
 };
