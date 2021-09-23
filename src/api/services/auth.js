b10c77d3967436f1b5a0b5ddb824c529877df35d
Lucas Godoi <lucazgodoy1996@gmail.com>
2021-09-23 16:35:56 -0300

    Add new message if empty token

--- src/api/services/auth.js
+++ src/api/services/auth.js
@@ -3,6 +3,9 @@ const jwt = require('jsonwebtoken');
 const SECRET = 'minhasupersenha';
 
 const validateToken = (authorization) => {
+    if (!authorization) {
+        return { obj: { message: 'missing auth token' }, status: 401 };
+    } 
     try {
         const user = jwt.verify(authorization, SECRET);
         return user;
