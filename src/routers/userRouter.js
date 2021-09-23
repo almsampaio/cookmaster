534a950ebe59ba0cb44611025e4eeb2b42e2f8c8
Guilherme Dugaich de Oliveira <guidugaich.dev@gmail.com>
2021-09-23 17:43:03 -0300

    admin route

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,9 +1,12 @@
 const { Router } = require('express');
+const { validateJWT } = require('../auth/authMiddleware');
 
 const router = Router();
 
 const userController = require('../controllers/userController');
 
 router.post('/', userController.addUser);
+
+router.post('/admin', validateJWT, userController.addAdmin);
 
 module.exports = router;
