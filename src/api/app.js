d1205fdfe333da5aaa01cc0ac8ea26d51192cb44
mallmann02 <leomallmann020302@gmail.com>
2021-09-23 17:46:42 -0300

    let app use bodyparser and design first route

--- src/api/app.js
+++ src/api/app.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const bodyparser = require('body-parser');
+
+const UserController = require('../controllers/userController');
 
 const app = express();
+
+app.use(bodyparser.json());
 
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
   response.send();
+  response.json({message: 'ff'})
 });
 // Não remover esse end-point, ele é necessário para o avaliador
+
+app.post('/users', UserController.create);
 
 module.exports = app;
