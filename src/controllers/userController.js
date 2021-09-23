27427e182efe6731724f1dd519d25f5d7a83b18a
Guilherme Dugaich de Oliveira <guidugaich.dev@gmail.com>
2021-09-23 17:43:31 -0300

    controller addAdmin

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,7 +2,33 @@ const userService = require('../services/userService');
 
 async function addUser(req, res) {
   const { name, email, password } = req.body;
-  const { code, message, user } = await userService.addUser({ name, email, password });
+  const { code, message, user } = await userService.addUser({
+    name,
+    email,
+    password,
+    role: 'user',
+  });
+
+  if (message) {
+    return res.status(code).json({ message });
+  }
+  res.status(code).json({ user });
+}
+
+async function addAdmin(req, res) {
+  const { name, email, password } = req.body;
+  const { role } = req;
+
+  if (role !== 'admin') {
+    return res.status(403).json({ message: 'Only admins can register new admins' });
+  }
+
+  const { code, message, user } = await userService.addUser({
+    name,
+    email,
+    password,
+    role: 'admin',
+  });
 
   if (message) {
     return res.status(code).json({ message });
@@ -12,4 +38,5 @@ async function addUser(req, res) {
 
 module.exports = {
   addUser,
+  addAdmin,
 };
