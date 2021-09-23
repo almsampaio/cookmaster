bf9e664cc509afd096d0d5fbc5887d969a52e192
lucas-paz <lucas.hpaz@outlook.com>
2021-09-23 18:54:38 -0300

    adiciona teste para admin

--- src/integration-tests/user.spec.js
+++ src/integration-tests/user.spec.js
@@ -5,27 +5,21 @@ const server = require('../api/app');
 const { MongoClient } = require('mongodb');
 const getConnection = require('./connection');
 
+const { } = require('./recipes.spec');
+
 chai.use(chaiHttp);
 const { expect } = chai;
 
-const VALID_NEW_USER = {
-  name: 'userName',
-  email: 'userName@email.com',
+const VALID_NEW_USER2 = {
+  name: 'userName2',
+  email: 'userName@email2.com',
   password: '12345678',
 };
-
-const INVALID_NEW_USER = {
-  name: 'userName',
-  password: '12345678',
-};
-
-const ADD_USER = async (userObj) => chai.request(server).post('/users').send(userObj);
-const LOGIN = async (payload) => chai.request(server).post('/login').send(payload);
 
 describe('POST /users', () => {
-  before( async () => {
+  before(async () => {
     const VirtualDB = await getConnection();
-    sinon.stub(MongoClient, 'connect').resolves(VirtualDB); 
+    sinon.stub(MongoClient, 'connect').resolves(VirtualDB);
   });
 
   after(() => {
@@ -34,10 +28,10 @@ describe('POST /users', () => {
 
   describe('Quando é criado com sucesso', () => {
     let response = {};
-    before( async () => {
+    before(async () => {
       response = await chai.request(server)
         .post('/users')
-        .send(VALID_NEW_USER);
+        .send(VALID_NEW_USER2);
     });
 
     it('retorna o código de status 201', () => {
@@ -50,7 +44,42 @@ describe('POST /users', () => {
       expect(response.body.user).to.have.property('name');
       expect(response.body.user).to.have.property('_id');
       expect(response.body.user).not.to.have.property('password');
-      expect(response.body.user.name).to.be.equal('userName');
+      expect(response.body.user.name).to.be.equal('userName2');
+    });
+  });
+});
+
+describe('POST /users/admin', () => {
+  before(async () => {
+    const VirtualDB = await getConnection();
+    sinon.stub(MongoClient, 'connect').resolves(VirtualDB);
+  });
+
+  after(() => {
+    MongoClient.connect.restore();
+  });
+
+  describe('Só outros admins podem criar um admin', async () => {
+    let response = {};
+    before(async () => {
+      await chai.request(server).post('/users').send(VALID_NEW_USER2);
+      const { body: { token } } = await chai.request(server)
+        .post('/login')
+        .send({ email: 'userName@email.com', password: '12345678' });
+      response = await chai.request(server)
+        .post('/users/admin')
+        .set('authorization', token)
+        .send(VALID_NEW_USER2);
+    });
+
+    it('Retorna codigo 403', async () => {
+      expect(response).to.have.status(403);
+    });
+
+    it('Retorna mensagem de erro', async () => {
+      expect(response).to.be.a('object');
+      expect(response.body).to.have.property('message');
+      expect(response.body.message).to.be.equal('Only admins can register new admins');
     });
   });
 });
