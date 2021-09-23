9914428451550dac83dab8376bfd862e00b19ab2
Tiago Santos <tiago881@gmail.com>
2021-09-23 16:17:01 -0300

    feat: validate inputs

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,14 +1,26 @@
 const userModel = require('../models/user-model');
+const userMiddleware = require('../middlewares/user-middleware');
+
+const validateEmail = async (email) => {
+  const findEmail = await userModel.findEmail(email);
+  if (findEmail.length >= 1) return { status: 409, message: 'Email already registered' };
+
+  return false;
+};
 
 const createUser = async (name, email, password) => {
+  const validateInput = userMiddleware.fieldRequired(name, email, password);
+  if (validateInput) return validateInput;
+
+  const validatingEmail = await validateEmail(email);
+
+  if (validatingEmail) return validatingEmail;
+
   const creteadUser = await userModel.createUser(name, email, password);
 
   return creteadUser;
 };
 
-const findEmail = async (email) => userModel.findEmail(email);
-
 module.exports = {
   createUser,
-  findEmail,
 };
