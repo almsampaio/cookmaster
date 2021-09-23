fb52640c060d148c28cbf016e430988c84065f4c
Mateus Alencar <mtauws@gmail.com>
2021-09-23 16:23:38 -0300

    fix: npm

--- src/integration-tests/usersIntegration.test.js
+++ src/integration-tests/usersIntegration.test.js
@@ -30,6 +30,60 @@ describe('POST /users/', () => {
         expect(response.body.message).to.be.equal('Invalid entries. Try again.');
       });
     });
+
+    describe('When fields are entered, but the email does not have the correct structure', () => {
+      let response;
+
+      before(async () => {
+        response = await chai.request(app).post('/users/')
+          .send({ name: 'Mateus', email: 'mateus', password: 'trybe2021' });
+      });
+
+      it('should return with HTTP 400 status', () => {
+        expect(response).to.have.status(400);
+      });
+
+      it('must return an object in the body with the key "message"', () => {
+        expect(response.body).to.be.an('object').to.have.property('message');
+      });
+
+      it('should return the correct error message', () => {
+        expect(response.body.message).to.be.equal('Invalid entries. Try again.');
+      });
+    });
+
+    describe('When the user already exists in the database', () => {
+      let connectionMock;
+      let response;
+
+      before(async () => {
+        connectionMock = await getConnection();
+        sinon.stub(MongoClient, 'connect').resolves(connectionMock);
+
+        await connectionMock.db('Cookmaster').collection('users')
+          .insertOne({ name: 'Mateus', email: 'mateus@betrybe.com', password: 'trybe2021' });
+
+        response = await chai.request(app).post('/users')
+          .send({ name: 'Mateus', email: 'mateus@betrybe.com', password: 'trybe2021' });
+      });
+
+      after(async () => {
+        MongoClient.connect.restore();
+        await connectionMock.db('Cookmaster').collection('users').deleteOne({ email: 'mateus@betrybe.com' });
+      });
+
+      it('should return with HTTP status 409', () => {
+        expect(response).to.have.status(409);
+      });
+
+      it('must return an object in the body with the key "message"', () => {
+        expect(response.body).to.be.an('object').to.have.property('message');
+      });
+
+      it('should return the correct error message', () => {
+        expect(response.body.message).to.be.equal('Email already registered');
+      });
+    });
   });
   describe('When we can successfully register a new user', () => {
     let connectionMock;
