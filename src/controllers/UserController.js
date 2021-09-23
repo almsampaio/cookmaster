52b572b36ad59d59e39276bb3cbdd5cc7ef96d4d
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-09-23 16:39:03 -0300

    feat: implementation controller for UserController function createAdmin

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -13,6 +13,25 @@ const create = async (req, res) => {
   res.status(201).json(userCreated);
 };
 
+const createAdmin = async (req, res) => {
+  const { name, email, password } = req.body;
+  const { user: { role } } = req;
+
+  if (role !== 'admin') {
+    return res.status(403).json({ message: 'Only admins can register new admins' }); 
+  }
+
+  const userCreated = await UserService.createAdmin({ name, email, password });
+
+  if (userCreated.message) {
+    return res.status(userCreated.code)
+      .json({ message: userCreated.message });
+  }
+
+  res.status(201).json(userCreated);
+};
+
 module.exports = {
   create,
+  createAdmin,
 };
