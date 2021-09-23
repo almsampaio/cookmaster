40d1a66d97a4514413b79b1eedac5c7d6b932310
Mateus Alencar <mtauws@gmail.com>
2021-09-23 16:20:28 -0300

    chore: req 11

---
+++ src/integration-tests/usersIntegration.test.js
@@ -0,0 +1,63 @@
+const chai = require('chai');
+const sinon = require('sinon');
+const app = require('../api/app');
+const { getConnection } = require('./mock');
+
+const chaiHttp = require('chai-http');
+const { MongoClient } = require('mongodb');
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('POST /users/', () => {
+  describe('When we make an unsuccessful request', () => {
+    describe('When the name, email or password fields are not informed', () => {
+      let response;
+
+      before(async () => {
+        response = await chai.request(app).post('/users/').send({});
+      });
+
+      it('should return with HTTP 400 status', () => {
+        expect(response).to.have.status(400);
+      });
+
+      it('must return an object in the body with the key "message"', () => {
+        expect(response.body).to.be.an('object').to.have.property('message');
+      });
+
+      it('should return the correct error message', () => {
+        expect(response.body.message).to.be.equal('Invalid entries. Try again.');
+      });
+    });
+  });
+  describe('When we can successfully register a new user', () => {
+    let connectionMock;
+    let response;
+
+    before(async () => {
+      connectionMock = await getConnection();
+      sinon.stub(MongoClient, 'connect').resolves(connectionMock);
+
+      response = await chai.request(app).post('/users')
+        .send({ name: 'Mateus', email: 'mateus@betrybe.com', password: 'trybe2021' });
+    });
+
+    after(async () => {
+      MongoClient.connect.restore();
+      await connectionMock.db('Cookmaster').collection('users').deleteOne({ email: 'mateus@betrybe.com' });
+    });
+
+    it('should return with HTTP status 201', () => {
+      expect(response).to.have.status(201);
+    });
+
+    it('must return an object in the body with the key "user"', () => {
+      expect(response.body).to.be.an('object').to.have.property('user');
+    });
+
+    it('must return an object with the correct data of the registered user', () => {
+      expect(response.body.user).to.be.an('object').to.have.all.keys('name', 'email', 'role', '_id');
+    });
+  });
+});
