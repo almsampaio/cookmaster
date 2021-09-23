6294d14f97b94f5431bfab11cd306d8f25581168
Paulo Vitor Borges <pvtrybe209@gmail.com>
2021-09-23 19:40:54 -0300

    fix:connetion

--- src/api/models/connection.js
+++ src/api/models/connection.js
@@ -1,20 +1,26 @@
 const { MongoClient } = require('mongodb');
 
-const OPTIONS = {
+require('dotenv').config();
+
+const MONGO_DB_URL = process.env.MONGO_DB_URL || 'mongodb://mongodb:27017/CookMaster';
+const DB_NAME = 'Cookmaster';
+let schema = null; 
+    
+  async function connection() {
+    if (schema) return Promise.resolve(schema);
+    return MongoClient
+    .connect(MONGO_DB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-};
+    })
+    .then((conn) => conn.db(DB_NAME))
+    .then((dbSchema) => {
+    schema = dbSchema;
+    return schema;
+    })
+    .catch((err) => {
+    console.error(err);
+    });
+    } 
 
-const MONGO_DB_URL = 'mongodb://127.0.0.1:27017';
-const DB_NAME = 'Cookmaster';
-let db = null;
-
-const connection = () => (db
-    ? Promise.resolve(db)
-    : MongoClient.connect(MONGO_DB_URL, OPTIONS)
-    .then((conn) => {
-    db = conn.db(DB_NAME);
-    return db;
-    }));
-
-module.exports = connection;
+module.exports = connection; 
