ed96d1e33ec77f81de12e7c87b2dc698ce0aee26
Tiago Santos <tiago881@gmail.com>
2021-09-23 16:49:33 -0300

    ci: environment variable

--- src/models/connection.js
+++ src/models/connection.js
@@ -1,24 +1,27 @@
 const { MongoClient } = require('mongodb');
 
-const MONGO_DB_URL = 'mongodb://localhost:27017'; // ou coloque sua URL do MongoDB aqui
-const DB_NAME = 'Cookmaster';
-let schema = null;
+require('dotenv').config();
 
-async function connection() {
-  if (schema) return Promise.resolve(schema);
-  return MongoClient
-    .connect(MONGO_DB_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-    .then((conn) => conn.db(DB_NAME))
-    .then((dbSchema) => {
-      schema = dbSchema;
-      return schema;
-    })
-    .catch((err) => {
-      console.error(err);
-    });
-}
+const MONGO_DB_URL = process.env.MONGO_DB_URL || 'mongodb://localhost:27017/Cookmaster'; 
+
+const OPTIONS = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+let db = null;
+
+const connection = () => (
+  db
+  ? Promise.resolve(db)
+  : MongoClient.connect(MONGO_DB_URL, OPTIONS)
+  .then((conn) => {
+    db = conn.db(process.env.DB_NAME);
+    return db;
+  }).catch((err) => {
+    console.error(err);
+    process.exit(1);
+  })
+);
 
 module.exports = connection;
