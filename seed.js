56e45346788421f9cce566c3de75027194e5f36c
AndreBarroso <andre_barroso17@hotmail.com>
2021-09-23 13:41:00 -0300

    feat: adiciona query do seed para cira admin

--- seed.js
+++ seed.js
@@ -1,5 +1,2 @@
 // colocar query do MongoDB
-// db.users.insertOne({ "name" : "Erick Jacquin", "email" : "erickjacquin@gmail.com", "password" : "12345678", "role" : "user" });
-
-// db.users.insertOne("name" : "Receita do Jacquin", "ingredients" : "Frango", "preparation" : "10 minutos no forno" });
-
+db.users.insertOne({ name: 'admin', email: 'root@email.com', password: 'admin', role: 'admin' });
--- src/api/models/connection.js
+++ src/api/models/connection.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require('mongodb');
 
-// const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster';
-const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
+const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster';
+// const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
 
 const DB_NAME = 'Cookmaster';
 
