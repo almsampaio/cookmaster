74a1a9634d64f5328eefc6d875dcb4ce44eed11c
Flavio Franco <flavio.win@gmail.com>
2021-09-23 08:24:29 -0300

    requisito 6

--- seed.js
+++ seed.js
@@ -1,6 +1,1 @@
-db.users.insertOne({
-  name: 'admin',
-  email: 'root@email.com',
-  password: 'admin',
-  role: 'admin',
-});
+db.users.insertOne({ name: 'admin', email: 'root@email.com', password: 'admin', role: 'admin' });
