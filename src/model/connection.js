21bb1702570f48cd156b298f1171baf956c22591
Willian Alves <willian.alvesb65@gmail.com>
2021-09-23 18:56:01 -0300

    Feat: conf. connection usando variavel de ambiente

---
+++ src/model/connection.js
@@ -0,0 +1,21 @@
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+
+const OPTIONS = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+};
+
+const MONGO_DB_URL = process.env.MONGO_DB_URL || 'mongodb://mongodb:27017/Cookmaster';
+
+let db = null;
+
+const connection = () => (db
+    ? Promise.resolve(db)
+    : MongoClient.connect(MONGO_DB_URL, OPTIONS)
+    .then((conn) => {
+    db = conn.db('model_example');
+    return db;
+    }));
+
+module.exports = connection;
