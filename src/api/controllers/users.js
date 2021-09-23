ebb8b0ed51d0af914e631ca8916eed99e30ebdbe
Lucas Pedroso <lepsk8@gmail.com>
2021-09-23 10:22:52 -0300

    feat: add userController

--- src/api/controllers/users.js
+++ src/api/controllers/users.js
@@ -1,8 +1,20 @@
 const route = require('express').Router();
 const rescue = require('express-rescue');
+const { StatusCodes } = require('http-status-codes');
+const InvalidEntriesTryAgain = require('../errors/InvalidEntriesTryAgain');
+const UserService = require('../services/userService');
 
 route.post('/', rescue(async (req, res) => {
+  const { error } = UserService.userSchema.validate(req.body);
+  if (error) throw new InvalidEntriesTryAgain();
 
+  const user = await UserService.createUser(req.body);
+  res.status(StatusCodes.CREATED).json({ user });
 }));
+
+route.use((err, _req, res, _next) => {
+  console.log(err.message);
+  return res.status(err.statusCode).json(err.err);
+});
 
 module.exports = (app) => app.use('/users', route);
