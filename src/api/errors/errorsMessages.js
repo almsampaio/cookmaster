53f894f57559358e842f51a3260e53bacd60dd88
Lucas Pedroso <lepsk8@gmail.com>
2021-09-23 10:27:57 -0300

    feat: add errors messages

---
+++ src/api/errors/errorsMessages.js
@@ -0,0 +1,13 @@
+const errorsMessages = {
+  emailAlreadyRegistered: 'Email already registered',
+  invalidEntriesTryAgain: 'Invalid entries. Try again.',
+  allFieldsMustBeFilled: 'All fields must be filled',
+  incorrectUsernameOrPassword: 'Incorrect username or password',
+  jwtMalformed: 'jwt malformed',
+  recipeNotFound: 'recipe not found',
+  missingAuthToken: 'missing auth token',
+};
+
+module.exports = {
+  errorsMessages,
+};
