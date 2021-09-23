b84685c488c494606db86b70eb625d8344dba591
Lucas Pedroso <lepsk8@gmail.com>
2021-09-23 10:34:40 -0300

    feat: add routes /users /login /recipes /images

--- src/api/app.js
+++ src/api/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const { route: staticImages } = require('./services/multerConfig');
 
 const app = express();
 
 app.use(express.json());
+app.use('/images', staticImages);
 
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
@@ -11,5 +13,7 @@ app.get('/', (request, response) => {
 // Não remover esse end-point, ele é necessário para o avaliador
 
 require('./controllers/users')(app);
+require('./controllers/login')(app);
+require('./controllers/recipes')(app);
 
 module.exports = app;
