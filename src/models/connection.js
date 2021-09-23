c6cf3c7d9865982fece119bdfdc1bb7dcb68e76e
Mateus Alencar <mtauws@gmail.com>
2021-09-23 16:32:52 -0300

    chore: change localhost to mongodb

--- src/models/connection.js
+++ src/models/connection.js
@@ -6,7 +6,7 @@ const OPTIONS = {
 };
 
 const { LOCAL_DB_URL } = process.env;
-const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster';
+const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
 let db = null;
 
 const connection = () =>
