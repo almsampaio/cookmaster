c15f128a224e7b1234169aa68814784ce54b101b
ScriptCamilo <script.camilo@gmail.com>
2021-09-23 08:34:25 -0300

    feat: creating token validation function

--- src/services/validations/index.js
+++ src/services/validations/index.js
@@ -1,16 +1,34 @@
+const jwt = require('jsonwebtoken');
 const Validation = require('./constructor');
 
 const usersValidation = require('./usersValidation');
 const loginValidation = require('./loginValidation');
 const recipesValidation = require('./recipesValidation');
 
+const SECRET = 'creatingtoken';
+
 function isNameValid(name) {
   const isNameTrue = new Validation(name);
   isNameTrue.verify('badRequest');
 }
 
+function isTokenValid(token) {
+  const errorMessage = 'jwt malformed';
+  const isTokenTrue = new Validation(!!token);
+  isTokenTrue.verify('unauthorized', errorMessage);  
+
+  try {
+    const decode = jwt.verify(token, SECRET);
+    return decode;
+  } catch (_err) {
+    const throwTokenError = new Validation(false);
+    throwTokenError.verify('unauthorized', errorMessage);
+  }
+}
+
 module.exports = {
   isNameValid,
+  isTokenValid,
   ...usersValidation,
   ...loginValidation,
   ...recipesValidation,
