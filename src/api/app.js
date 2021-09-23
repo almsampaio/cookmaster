c7bf1116c52a284c788d9a1ec9088b53d2ef6927
Tiago Santos <tiago881@gmail.com>
2021-09-23 10:32:44 -0300

    ci: import route file and body-parser lib

--- src/api/app.js
+++ src/api/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+const route = require('../router/routes');
 
 const app = express();
+app.use(bodyParser.json());
+app.use(route);
 
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
