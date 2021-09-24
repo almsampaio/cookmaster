132db34ad9e69ad6b1c9c43b27c52a34daf0bec5
Sergio Martins <sergiomos2010@gmail.com>
2021-09-23 23:11:34 -0300

    Fix: remove password from response

--- src/useCases/createUser/createUserService.js
+++ src/useCases/createUser/createUserService.js
@@ -3,7 +3,7 @@ const createUserModel = require('./createUserModel');
 
 const createUserService = async (userInfo) => {
     const user = new User(userInfo);
-    const { _password, ...createdUser } = await createUserModel(user);
+    const { password, ...createdUser } = await createUserModel(user);
     return createdUser;
 };
 
