ab06f878246409698128e5b4571a4f3974634884
AdelinoJnr <adelinojunior96@gmail.com>
2021-09-24 13:26:35 -0300

    feat: test POST /users

--- src/api/server.js
+++ src/api/server.js
@@ -3,5 +3,3 @@ const app = require('./app');
 const { PORT } = require('../data');
 
 app.listen(PORT, () => console.log(`conectado na porta ${PORT}`));
-
-module.exports = PORT;
---
+++ src/integration-tests/user.spec.js
@@ -0,0 +1,111 @@
+const { MongoClient } = require('mongodb');
+const chai = require('chai');
+const sinon = require('sinon');
+
+const { getConnection } = require('./connectionMock');
+const server = require('../api/app');
+
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Criacao de um usuario POST /users', () => {
+
+  describe('Quando cria um usuario com sucesso', () => {
+    let response;
+
+    before(async () => {
+      const connectionMock = await getConnection();
+
+      sinon.stub(MongoClient, 'connect')
+        .resolves(connectionMock);
+
+      response = await chai.request(server)
+        .post('/users')
+        .send({
+          "name": "Adelino Junior",
+          "email": "adelino@gmail.com",
+          "password": "123456"
+        });
+    });
+
+    after(async () => {
+      MongoClient.connect.restore();
+    });
+
+    it('Esperar que o retorno do body tenha a propriedade "user"', () => {
+      expect(response.body).to.be.property('user');
+      expect(response.body).to.be.an('object');
+    })
+    it('Esperar que o retorno do body.user tenha a propriedade "name"', () => {
+      expect(response.body.user).to.be.property('name');
+      expect(response.body.user).to.be.equal('Adelino Junior');
+    })
+    it('Esperar que o retorno do body.user tenha a propriedade "email"', () => {
+      expect(response.body.user).to.be.property('email');
+      expect(response.body.user).to.be.equal('adelino@gmail.com');
+    })
+    it('Esperar que o retorno do body.user tenha a propriedade "_id"', () => {
+      expect(response.body.user).to.be.property('_id');
+      expect(response.body.user).to.be.an('string');
+    })
+    it('Esperar que o retorno do body.user tenha a propriedade "role"', () => {
+      expect(response.body.user).to.be.property('role');
+      expect(response.body.user).to.be.equal('user');
+    })
+    it('Espera que o status seja "201"', () => {
+      expect(response).to.have.status(201);
+    })
+  });
+  describe('Erro na criacao de usuario', () => {
+    let response;
+
+    before(async () => {
+      const connectionMock = await getConnection();
+
+      sinon.stub(MongoClient, 'connect')
+        .resolves(connectionMock);
+
+      // response = await chai.request(server)
+      //   .post('/users')
+      //   .send({
+      //     "name": "Adelino Junior",
+      //     "email": "adelino@gmail.com",
+      //     "password": "123456"
+      //   });
+    });
+
+    after(async () => {
+      MongoClient.connect.restore();
+    });
+
+    it('Espera que o campo "name" seja passado', async () => {
+      response = await chai.request(server)
+        .post('/users')
+        .send({ email: 'adelinojunior@gmail.com', password: '123456' });
+
+      expect(response).to.have.status(400);
+      expect(response.body).to.be.property('message');
+      expect(response.body.message).to.be.equal('Invalid entries. Try again.');
+    })
+    it('Espera que o campo "password" seja passado', async () => {
+      response = await chai.request(server)
+        .post('/users')
+        .send({ email: 'adelinojunior@gmail.com', name: 'Adelino Junior' });
+
+      expect(response).to.have.status(400);
+      expect(response.body).to.be.property('message');
+      expect(response.body.message).to.be.equal('Invalid entries. Try again.');
+    })
+    it('Espera que o campo "email" seja passado', async () => {
+      response = await chai.request(server)
+        .post('/users')
+        .send({ password: '123456', name: 'Adelino Junior' });
+
+      expect(response).to.have.status(400);
+      expect(response.body).to.be.property('message');
+      expect(response.body.message).to.be.equal('Invalid entries. Try again.');
+    })
+  });
+});
