7e71396e3d39adba57c2632a0c5b171c94176ea5
Maria Baeta <m.martinsdg@gmail.com>
2021-09-24 17:08:14 -0300

    feat: :sparkles: Requisito 10 - Acessa a imagem de uma receita

--- src/api/app.js
+++ src/api/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 const routerUser = require('./routerUser');
 const routerRecipes = require('./routerRecipes');
 const routerAdmin = require('./routerAdmin');
@@ -14,6 +15,8 @@ app.use('/users', routerUser.Userrouter);
 app.use('/users', routerAdmin.Adminrouter);
 app.use('/login', routerUser.Authentication);
 app.use('/recipes', routerRecipes.Recipesrouter);
+// https://qastack.com.br/programming/5924072/express-js-cant-get-my-static-files-why
+app.use('/images', express.static(path.join(__dirname, '..', 'uploads')));
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
   response.send();
