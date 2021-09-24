124a8b9b1c479277cbc73501c219a1ce17ff4836
PollyanaOliveira <pollyana.deoliveir@gmail.com>
2021-09-23 23:18:11 -0400

    feat: :sparkles: req 10

--- src/api/app.js
+++ src/api/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 const router = require('./routers');
 
 const app = express();
 app.use(bodyParser.json());
 
 app.use(router);
+app.use('/images', express.static(path.join(__dirname, '..', 'uploads')));
 
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
