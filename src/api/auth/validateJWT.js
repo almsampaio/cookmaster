f6e921b5b8edd63e891e169a229caf2953e7729a
mallmann02 <leomallmann020302@gmail.com>
2021-09-23 21:09:58 -0300

    JWT validator middleware

---
+++ src/api/auth/validateJWT.js
@@ -0,0 +1,48 @@
+// validateJWT.js
+const jwt = require('jsonwebtoken');
+const userModel = require('../../models/userModel');
+
+/* Mesma chave privada que usamos para criptografar o token.
+   Agora, vamos usá-la para descriptografá-lo.
+   Numa aplicação real, essa chave jamais ficaria hardcoded no código assim,
+   e muitos menos de forma duplicada, mas aqui só estamos interessados em
+   ilustrar seu uso ;) */
+const secret = 'secret123';
+
+module.exports = async (req, res, next) => {
+  const token = req.headers['authorization'];
+
+  if (!token) {
+    return res.status(401).json({ error: 'Token não encontrado' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, secret);
+    /*
+      A variável decoded será um objeto equivalente ao seguinte:
+      {
+        data: {
+          _id: '5e54590ba49448f7e5fa73c0',
+          username: 'italssodj',
+          password: 'senha123'
+        },
+        iat: 1582587327,
+        exp: 1584774714908
+      }
+    */
+
+    const user = await userModel.findUserByEmail(decoded.data.email);
+
+    if (!user) {
+      return res
+        .status(401)
+        .json({ message: 'Erro ao procurar usuário do token.' });
+    }
+
+    req.user = user;
+
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: err.message });
+  }
+};
