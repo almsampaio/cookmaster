fbc374ebfd0593026bc4ab5feefe328ff462da9d
Edrei <edreimateus@gmail.com>
2021-09-25 01:15:54 -0300

    feat: 10 requiriment

--- src/api/app.js
+++ src/api/app.js
@@ -23,6 +23,8 @@ const { authMiddleware } = require('./middlewares/AuthMiddleware');
 const app = express();
 app.use(bodyParser.json());
 
+app.use('/images', express.static(path.join(__dirname, '..', 'uploads')));
+
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
   response.send();
