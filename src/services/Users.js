d0292eec230ced5a3808804a88ec02eafb6fcad6
lucas-chave <lcchavesr150@gmail.com>
2021-09-26 19:20:48 -0300

    adding mandatory verification of a user's fields

--- src/services/Users.js
+++ src/services/Users.js
@@ -2,7 +2,16 @@ const {
   create,
 } = require('../models/Users');
 
+const verifyDataUser = (name, email, password) => {
+  if (!name || !email || !password) return { status: 400, message: 'Invalid entries. Try angain' };
+  return false;
+};
+
 const addUser = async (name, email, password) => {
+  const verifyUser = verifyDataUser(name, email, password);
+
+  if (verifyUser) return verifyDataUser(name, email, password);
+
   const result = await create(name, email, password);
 
   return result;
