4441f56ba98590562d7550cda44d86c8f38b8f7a
lucas-chave <lcchavesr150@gmail.com>
2021-09-26 18:53:31 -0300

    fixing function that adds user in controller layer

--- src/controllers/Users.js
+++ src/controllers/Users.js
@@ -2,9 +2,9 @@ const {
   addUser,
 } = require('../services/Users');
 
-const createUSer = (req, res) => {
+const createUSer = async (req, res) => {
   const { name, email, password } = req.body;
-  const result = addUser(name, email, password);
+  const result = await addUser(name, email, password);
 
   res.status(201).json(result);
 };
