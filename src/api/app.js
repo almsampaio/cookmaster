5d8c73af07e611a3d8235e5bed9336c9419e221a
lucas-chave <lcchavesr150@gmail.com>
2021-09-26 18:25:15 -0300

    creating route for adding user

--- src/api/app.js
+++ src/api/app.js
@@ -2,10 +2,16 @@ const express = require('express');
 
 const app = express();
 
+const {
+  createUSer,
+} = require('../controllers/Users');
+
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
   response.send();
 });
 // Não remover esse end-point, ele é necessário para o avaliador
+
+app.post('/users', createUSer);
 
 module.exports = app;
