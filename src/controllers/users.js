2caec90288f7ceda08d0527f79ad2a32ff8d47a9
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2021-09-27 04:16:02 -0300

    feat: requirement 1

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -2,7 +2,7 @@ const userService = require('../services/users');
 
 async function create(req, res) {
   const { name, email, password } = req.body;
-  const user = await userService.create(name, email, password);
+  const { user } = await userService.create(name, email, password);
   return res.status(201).json({ user });
 }
 
---
+++ src/models/login.js
@@ -0,0 +1,7 @@
+// const getConnection = require('./connections');
+
+// const createToken = require('../middlewares/token');
+
+// async function userLogin(email) {
+//   const db = await getConnection();
+// }
