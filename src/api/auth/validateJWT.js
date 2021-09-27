d9e9b7139d4c4a17f331a6e4094b24c83abb5395
mallmann02 <leomallmann020302@gmail.com>
2021-09-27 19:21:19 -0300

    fix: token not found error message

--- src/api/auth/validateJWT.js
+++ src/api/auth/validateJWT.js
@@ -13,7 +13,7 @@ module.exports = async (req, res, next) => {
   const token = req.headers['authorization'];
 
   if (!token) {
-    return res.status(401).json({ error: 'missing auth token' });
+    return res.status(401).json({ message: 'missing auth token' });
   }
 
   try {
