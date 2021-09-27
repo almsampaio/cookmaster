9aabdd4c364fddcd6d2b0433f92925bba61020d1
lucas-chave <lcchavesr150@gmail.com>
2021-09-27 15:14:53 -0300

    login function that returns a token

--- src/services/Users.js
+++ src/services/Users.js
@@ -1,7 +1,11 @@
+const jwt = require('jsonwebtoken');
 const { 
   create,
   getAll,
+  getByEmail,
 } = require('../models/Users');
+
+const SECRET = 'minhasenhasecreta';
 
 const verifyDataUser = (name, email, password) => {
   const regexEmail = /\S+@\S+\.\S+/;
@@ -12,6 +16,24 @@ const verifyDataUser = (name, email, password) => {
   return false;
 };
 
+const createLogin = async (email, password) => {
+  const regexEmail = /\S+@\S+\.\S+/;
+  if (!email || !password) return { message: 'All fields must be filled', status: 401 };
+  if (!regexEmail.test(email) || password.length < 8) {
+    return { message: 'Incorrect username or password', status: 401 };
+  }
+  const emailSearch = await getByEmail(email);
+
+  const { password: _, name: a, ...userPayload } = emailSearch;
+
+  const token = jwt.sign(userPayload, SECRET, {
+    algorithm: 'HS256',
+    expiresIn: '10d',
+  });
+
+  return { status: 200, token };
+};
+
 const addUser = async (name, email, password) => {
   const verifyUser = verifyDataUser(name, email, password);
   if (verifyUser) return verifyUser;
@@ -20,10 +42,10 @@ const addUser = async (name, email, password) => {
   if (bool) return { status: 409, message: 'Email already registered' };
 
   const result = await create(name, email, password);
-
   return result;
 };
 
 module.exports = {
   addUser,
+  createLogin,
 };
