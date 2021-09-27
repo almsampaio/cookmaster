d1e965ee0dd8657ee6a6b7f2fcfefa4bec411202
luizfelipe-dev <thelastnailmusic@gmail.com>
2021-09-27 13:41:05 -0300

    upload image path fixed for evaluator

--- src/api/middlewares/upload.js
+++ src/api/middlewares/upload.js
@@ -2,7 +2,8 @@ const multer = require('multer');
 const path = require('path');
 
 const storage = multer.diskStorage({
-    destination: (req, file, callback) => callback(null, path.join(__dirname, '..', '..', 'uploads')),
+    destination: (req, file, callback) => callback(null, path
+        .join(__dirname, '..', '..', 'uploads')),
     filename: (req, file, callback) => callback(null, `${req.params.id}.jpeg`),
 });
 
