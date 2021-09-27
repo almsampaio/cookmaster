f465c4464b85d3817ec887775f4ed7bcc02da735
lucas-chave <lcchavesr150@gmail.com>
2021-09-26 23:38:40 -0300

    adding email spell check

--- src/services/Users.js
+++ src/services/Users.js
@@ -1,17 +1,23 @@
 const { 
   create,
+  getAll,
 } = require('../models/Users');
 
 const verifyDataUser = (name, email, password) => {
-  if (!name || !email || !password) return { status: 400, message: 'Invalid entries. Try again.' };
-
+  const regexEmail = /\S+@\S+\.\S+/;
+  if (!name || !email || !password || !regexEmail.test(email)) {
+    return { 
+      status: 400, message: 'Invalid entries. Try again.' };
+    }
   return false;
 };
 
 const addUser = async (name, email, password) => {
   const verifyUser = verifyDataUser(name, email, password);
+  if (verifyUser) return verifyUser;
 
-  if (verifyUser) return verifyDataUser(name, email, password);
+  const bool = (await getAll()).some((user) => user.email === email);
+  if (bool) return { status: 409, message: 'Email already registered' };
 
   const result = await create(name, email, password);
 
