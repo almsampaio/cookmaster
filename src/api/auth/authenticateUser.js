151b29f20a86e163c63ee676406497098cf8a92f
mallmann02 <leomallmann020302@gmail.com>
2021-09-27 19:21:51 -0300

    feat(auth): create user authentication

---
+++ src/api/auth/authenticateUser.js
@@ -0,0 +1,16 @@
+const userModel = require('../../models/userModel');
+const recipeModel = require('../../models/recipeModel');
+
+module.exports = async (req, res, next) => {
+  const { user } = req;
+  const { id } = req.params;
+  const { userId } = await recipeModel.find(id);
+  
+  if (user._id !== userId && user.role !== 'admin') return res
+    .status(401)
+    .json({
+      message: 'This user cannot perform this action'
+    });
+  
+  next();
+};
