f024f7b3048031c21b5be58f1f3b92bd368ed08a
Joao Nasc <oi@joaonasc.dev>
2021-09-27 12:30:47 -0300

    feat: route to update recipe with an image

--- src/api/routes/recipes.js
+++ src/api/routes/recipes.js
@@ -1,7 +1,11 @@
 const routes = require('express').Router();
+const multer = require('multer');
 const recipesController = require('../controllers/recipes');
 const auth = require('../middlewares/auth');
 
+const upload = multer({ dest: 'uploads' });
+
+routes.put('/:id/image', auth, upload.single('image'), recipesController.update);
 routes.get('/:id', recipesController.get);
 routes.put('/:id', auth, recipesController.update);
 routes.delete('/:id', auth, recipesController.delete);
