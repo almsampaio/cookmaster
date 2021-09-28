3a71ded838fcde125595515b6533bc9017b78cd8
mallmann02 <leomallmann020302@gmail.com>
2021-09-28 16:00:54 -0300

    configure multer and create endpoint to add image

--- src/api/app.js
+++ src/api/app.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const bodyparser = require('body-parser');
+const multer = require('multer');
 
 const UserController = require('../controllers/userController');
 const RecipeController = require('../controllers/recipeController');
 const Login = require('../controllers/login');
 const validateJWT = require('../api/auth/validateJWT');
-const authenticateUser = require('../api/auth/authenticateUser');
 
 const app = express();
 
 app.use(bodyparser.json());
+
+const storage = multer.diskStorage({
+  destination: (_req, _file, callback) => {
+    callback(null, 'src/uploads');
+  },
+  filename: (req, _file, callback) => {
+    callback(null, `${req.params.id}.jpeg`);
+  }});
+
+const upload = multer({ storage });
 
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
@@ -19,6 +29,7 @@ app.get('/', (request, response) => {
 
 app.post('/users', UserController.create);
 app.post('/login', Login);
+app.put('/recipes/:id/image', validateJWT, upload.single('image'), RecipeController.addImage);
 app.delete('/recipes/:id', validateJWT, RecipeController.remove);
 app.get('/recipes/:id', RecipeController.find);
 app.put('/recipes/:id', validateJWT, RecipeController.edit);
