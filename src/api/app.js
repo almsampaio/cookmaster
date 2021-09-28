70a39a5d3942f25407f69beb485a054326dd7e03
Gustavo Sousa <gustavohl98@gmail.com>
2021-09-28 09:33:44 -0300

    Req 10 - Serve arquivos estaticos em /images/:id.jpeg

--- src/api/app.js
+++ src/api/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
 const errorHandler = require('./controllers/errorController');
 const userRouter = require('./routes/userRoutes');
@@ -8,6 +9,7 @@ const authController = require('./controllers/authController');
 const app = express();
 
 app.use(bodyParser.json());
+app.use('/images', express.static(path.join(__dirname, '..', 'uploads')));
 
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
