747c45225da9e4e89c9c91e654076d9200d2e496
mallmann02 <leomallmann020302@gmail.com>
2021-09-28 18:29:36 -0300

    change database connection URL

--- src/models/connection.js
+++ src/models/connection.js
@@ -5,7 +5,7 @@ const OPTIONS = {
   useUnifiedTopology: true,
 };
 
-const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster';
+const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
 const DB_NAME = 'Cookmaster';
 
 let db = null;
