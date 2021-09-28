665f298f27c5a5f6c0d51c98669af9112ccf89a4
Sergio Martins <sergiomos2010@gmail.com>
2021-09-28 15:50:19 -0300

    Fix: req 9

--- src/useCases/createRecipe/createRecipeModel.js
+++ src/useCases/createRecipe/createRecipeModel.js
@@ -3,20 +3,15 @@ const recipeCollection = require('../../database/recipesCollection');
 
 const insertImagePath = async (recipeId) => {
   const collection = await recipeCollection();
-  const recipe = await collection.aggregate([
-    { $match: { _id: ObjectId(recipeId) } },
-    { $addFields: {
-      image: { $concat: ['localhost:3000/src/uploads/', recipeId, '.jpeg'] },
-    } },
-  ]).toArray();
-  
-  return recipe;
+  const image = `localhost:3000/src/uploads/${recipeId}.jpeg`;
+  await collection.updateOne({ _id: ObjectId(recipeId) }, { $set: { image } });
 };
 
 const createRecipeModel = async (recipe) => {
   const collection = await recipeCollection();
-  const { ops: [{ _id }] } = await collection.insertOne(recipe);
-  const createdRecipe = await insertImagePath(_id.toString());
+  const { ops: [createdRecipe] } = await collection.insertOne(recipe);
+  const { _id } = createdRecipe;
+  await insertImagePath(_id.toString());
 
   return createdRecipe;
 };
