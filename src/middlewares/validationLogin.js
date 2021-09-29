4a60c3a727ea598881e6b985ac1b856ec4cf6097
samuelmmjr <samuelmmjunior@gmail.com>
2021-09-29 13:03:23 -0300

    feat req6

--- src/middlewares/validationLogin.js
+++ src/middlewares/validationLogin.js
@@ -9,34 +9,34 @@ const emailRequired = async (req, res, next) => {
     next();
 };
 
-const passwordRequired = async (request, response, next) => {
-    const { password } = request.body;
+const passwordRequired = async (req, res, next) => {
+    const { password } = req.body;
     
     if (!password || password === '') {
-        return response.status(401).json({ message: 'All fields must be filled' });
+        return res.status(401).json({ message: 'All fields must be filled' });
     }
 
     next();
 };
 
-const emailValid = async (request, response, next) => {
-    const { email } = request.body;
+const emailValid = async (req, res, next) => {
+    const { email } = req.body;
 
     const pattern = /\b[\w.-]+@[\w.-]+\.\w{2,4}\b/gi;
     const comparePattern = email.match(pattern);
     if (!comparePattern) {
-        return response.status(401).json({ message: 'Incorrect username or password' });
+        return res.status(401).json({ message: 'Incorrect username or password' });
     }
 
     next();
 };
 
-const passwordValid = async (request, response, next) => {
-    const { password } = request.body;
+const passwordValid = async (req, res, next) => {
+    const { password } = req.body;
 
     const searchByPassword = await usermodels.findPassword(password);
     if (!searchByPassword) {
-        return response.status(401).json({ message: 'Incorrect username or password' });
+        return res.status(401).json({ message: 'Incorrect username or password' });
 }
 
 next();
