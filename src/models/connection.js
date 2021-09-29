f05bbd890bee151de60f1aa5f39fa4d590c7af21
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-09-29 18:58:03 -0300

    Trocando link para o avaliador rodar

--- src/models/connection.js
+++ src/models/connection.js
@@ -6,10 +6,10 @@ const OPTIONS = {
 };
 
 // Para rodar localmente
- const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster';
+// const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster';
 
 // Para rodar no avaliador do github
-// const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
+ const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
 
 const DB_NAME = 'Cookmaster';
 
