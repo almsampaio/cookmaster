664aec6003f6176c3fbf627442e19e4b21d33db5
samuelmmjr <samuelmmjunior@gmail.com>
2021-09-29 12:12:11 -0300

    feat: req3 correçao lint no  app

--- src/api/app.js
+++ src/api/app.js
@@ -31,7 +31,7 @@ const {
 const { userLogin } = require('../controllers/login');
 
 app.use(bodyParser.json());
-// app.use('/images', express.static(path.join(__dirname, '..', 'uploads')));
+app.use('/images', express.static(path.join(__dirname, '..', 'uploads')));
 
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
