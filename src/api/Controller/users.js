ba231c1c64b75941908e877a1d9d81336e0e7ec6
gabrieldeori <deori.pessoal@gmail.com>
2021-09-29 20:45:04 -0300

    Minima mudança de espaçamento de linhas

--- src/api/Controller/users.js
+++ src/api/Controller/users.js
@@ -3,9 +3,11 @@ const Service = require('../Service');
 async function register(req, res, next) {
   const { name, email, password } = req.body;
   const userToRegister = { name, email, password, role: 'user' };
+
   const serviceResponse = await Service.users.register(userToRegister);
   if (serviceResponse.error) return next(serviceResponse);
   const { statusCode, payload } = serviceResponse;
+
   return res.status(statusCode).json(payload);
 }
 
