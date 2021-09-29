1c47f26ee7606295073f17b5626d7a7a9e162cc5
Perycles reis <perycles_reis@hotmail.com>
2021-09-29 20:24:31 -0300

    refactoring

--- src/models/Connection.js
+++ src/models/Connection.js
@@ -6,8 +6,9 @@ const OPTIONS = {
   useUnifiedTopology: true,
 };
 // const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster';
-// const { DB_NAME } = process.env;
-const { MONGO_DB_URL, DB_NAME } = process.env;
+const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
+const { DB_NAME } = process.env;
+// const { MONGO_DB_URL, DB_NAME } = process.env;
 
 let db = null;
 
