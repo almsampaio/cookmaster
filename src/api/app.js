19c0adf30c37ac561b93cee3e47cc37ff9a4daf8
gabrieldeori <deori.pessoal@gmail.com>
2021-09-29 19:05:37 -0300

    Refatorando 100%, pra ficar perfeito, em 19 horas?

--- src/api/app.js
+++ src/api/app.js
@@ -3,9 +3,7 @@ const express = require('express');
 const app = express();
 
 // Não remover esse end-point, ele é necessário para o avaliador
-app.get('/', (request, response) => {
-  response.send();
-});
+app.get('/', (_req, res) => res.send());
 // Não remover esse end-point, ele é necessário para o avaliador
 
 module.exports = app;
