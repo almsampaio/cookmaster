e95f31196e0fa9fe7d5b0e07e66cee3a28744998
williamDieter88 <williamzdieter@gmail.com>
2021-09-29 23:07:28 -0300

    80%?

--- src/api/models/connection.js
+++ src/api/models/connection.js
@@ -1,10 +1,10 @@
 const { MongoClient } = require('mongodb');
 
-const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster';
-const DB_NAME = 'Cookmaster';
-
-// const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
+// const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster';
 // const DB_NAME = 'Cookmaster';
+
+const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
+const DB_NAME = 'Cookmaster';
 
 const connection = () => MongoClient
     .connect(MONGO_DB_URL, {
