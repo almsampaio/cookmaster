9a229be9ab30b921bd332018c58bef1378bbd7b7
Willian Mendes <willianmendesbr@gmail.com>
2021-10-05 17:42:18 -0300

    test: req 11 agora vai

--- src/integration-tests/users.spec.js
+++ src/integration-tests/users.spec.js
@@ -80,14 +80,14 @@ describe('POST /login', () => {
 
             await usersCollection.insertOne({
                 name: 'Willian',
-                email: 'willian@gmail.com',
+                email: 'willian2@gmail.com',
                 password: '123456789',
                 role: 'user',
             })
 
             response = await chai.request(app)
                 .post('/login')
-                .send({email: 'willian@gmail.com', password: '123456789'});
+                .send({email: 'willian2@gmail.com', password: '123456789'});
         });
 
         it('retorna o código de status 200', () => {
@@ -218,6 +218,246 @@ describe('POST /users', () => {
             expect(response.body.user.name).to.be.equal("Willian");
             expect(response.body.user.email).to.be.equal("willian3@gmail.com");
             expect(response.body.user.role).to.be.equal("user");
+        });
+    });
+});
+
+describe('POST /users/admin', () => {
+    let connectionMock;
+
+    before(async () => {
+        connectionMock = await getConnection();
+        sinon.stub(MongoClient, 'connect').resolves(connectionMock);
+    });
+
+    after(() => {
+        MongoClient.connect.restore();
+    })
+
+    describe('Token vazio', () => {
+        let response;
+
+        before(async () => {
+            response = await chai.request(app).post('/users/admin').send({});
+        });
+
+        it('retorna o código de status 401', () => {
+            expect(response).to.have.status(401);
+        });
+
+        it('retorna um objeto', () => {
+            expect(response.body).to.be.a('object');
+        });
+
+        it('o objeto possui a propriedade "message"', () => {
+            expect(response.body).to.have.property('message');
+        });
+
+        it('a propriedade "message" possui o texto "missing auth token"',() => {
+            expect(response.body.message).to.be.equal('missing auth token');
+        });
+    });
+
+    describe('Token invalido', () => {
+        let response;
+
+        before(async () => {
+            const usersCollection = connectionMock.db('Cookmaster').collection('users');
+
+            const userLogged = {
+                name: 'Willian',
+                email: 'willian@gmail.com',
+                password: '123456789',
+                role: 'admin',
+            }
+
+            const newUser = {}
+
+            await usersCollection.insertOne(userLogged)
+
+            const token = await chai.request(app)
+                .post('/login')
+                .send({
+                    email: userLogged.email,
+                    password: userLogged.password
+                })
+
+            response = await chai.request(app)
+                .post('/users/admin')
+                .set({ Authorization: token })
+                .send(newUser);
+        });
+
+        it('retorna o código de status 401', () => {
+            expect(response).to.have.status(401);
+        });
+
+        it('retorna um objeto', () => {
+            expect(response.body).to.be.a('object');
+        });
+
+        it('o objeto possui a propriedade "message"', () => {
+            expect(response.body).to.have.property('message');
+        });
+
+        it('a propriedade "message" possui o texto "jwt malformed"',() => {
+            expect(response.body.message).to.be.equal('jwt malformed');
+        });
+    });
+
+    describe('Token valido e dados vazios', () => {
+        let response;
+
+        before(async () => {
+            const usersCollection = connectionMock.db('Cookmaster').collection('users');
+
+            const userLogged = {
+                name: 'Willian',
+                email: 'willian@gmail.com',
+                password: '123456789',
+                role: 'admin',
+            }
+
+            const newUser = {}
+
+            await usersCollection.insertOne(userLogged)
+
+            const tokenResponse = await chai.request(app)
+                .post('/login')
+                .send({
+                    email: userLogged.email,
+                    password: userLogged.password
+                })
+
+            response = await chai.request(app)
+                .post('/users/admin')
+                .set({ Authorization: tokenResponse.body.token })
+                .send(newUser);
+        });
+
+        it('retorna o código de status 400', () => {
+            expect(response).to.have.status(400);
+        });
+
+        it('retorna um objeto', () => {
+            expect(response.body).to.be.a('object');
+        });
+
+        it('o objeto possui a propriedade "message"', () => {
+            expect(response.body).to.have.property('message');
+        });
+
+        it('a propriedade "message" possui o texto "Invalid entries. Try again."',() => {
+            expect(response.body.message).to.be.equal('Invalid entries. Try again.');
+        });
+    });
+
+    describe('Token valido e email ja cadastrado', () => {
+        let response;
+
+        before(async () => {
+            const usersCollection = connectionMock.db('Cookmaster').collection('users');
+
+            const userLogged = {
+                name: 'Willian',
+                email: 'willian@gmail.com',
+                password: '123456789',
+                role: 'admin',
+            }
+
+            const newUser = {
+                name: 'Zeus',
+                email: 'Zeus@olimpo.com',
+                password: '123456789',
+                role: 'admin',
+            }
+
+            await usersCollection.insertMany([newUser, userLogged])
+
+            const tokenResponse = await chai.request(app)
+                .post('/login')
+                .send({
+                    email: userLogged.email,
+                    password: userLogged.password
+                })
+
+            response = await chai.request(app)
+                .post('/users/admin')
+                .set({ Authorization: tokenResponse.body.token })
+                .send(newUser);
+        });
+
+        it('retorna o código de status 409', () => {
+            expect(response).to.have.status(409);
+        });
+
+        it('retorna um objeto', () => {
+            expect(response.body).to.be.a('object');
+        });
+
+        it('o objeto possui a propriedade "message"', () => {
+            expect(response.body).to.have.property('message');
+        });
+
+        it('a propriedade "message" possui o texto "Email already registered"',() => {
+            expect(response.body.message).to.be.equal('Email already registered');
+        });
+    });
+
+    describe('Token valido e dados corretos', () => {
+        let response;
+
+        before(async () => {
+            const usersCollection = connectionMock.db('Cookmaster').collection('users');
+
+            const userLogged = {
+                name: 'Willian',
+                email: 'willian@gmail.com',
+                password: '123456789',
+                role: 'admin',
+            }
+
+            const newUser = {
+                name: 'Hercules',
+                email: 'hercules@olimpo.com',
+                password: '123456789',
+                role: 'admin',
+            }
+
+            await usersCollection.insertOne(userLogged)
+
+            const tokenResponse = await chai.request(app)
+                .post('/login')
+                .send({
+                    email: userLogged.email,
+                    password: userLogged.password
+                })
+
+            response = await chai.request(app)
+                .post('/users/admin')
+                .set({ Authorization: tokenResponse.body.token })
+                .send(newUser);
+        });
+
+        it('retorna o código de status 201', () => {
+            expect(response).to.have.status(201);
+        });
+
+        it('retorna um objeto', () => {
+            expect(response.body).to.be.a('object');
+        });
+
+        it('o objeto possui a propriedade "user"', () => {
+            expect(response.body).to.have.property('user');
+            expect(response.body.user).to.have.property('name');
+            expect(response.body.user).to.have.property('email');
+            expect(response.body.user).to.have.property('role');
+        });
+
+        it('a propriedade "user" possui o resultado:',() => {
+            expect(response.body.user.name).to.be.equal('Hercules');
+            expect(response.body.user.email).to.be.equal('hercules@olimpo.com');
+            expect(response.body.user.role).to.be.equal('admin');
         });
     });
 });
