2f7ce359000f77d193fdb52bd2c6e5cc270dfee3
jlfagundes <jlfagundes@gmail.com>
2021-10-12 16:58:29 -0300

    Feat: challenge 1  - create endpoint POST users

--- src/api/app.js
+++ src/api/app.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const controllerUsers = require('../controllers/users');
 
 const app = express();
+
+app.use(bodyParser.json());
 
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
   response.send();
 });
 // Não remover esse end-point, ele é necessário para o avaliador
+
+app.post('/users', controllerUsers.usersList);
 
 module.exports = app;
