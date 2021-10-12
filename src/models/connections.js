5fb47b73cd058e5321825aae5cd862e5639b4c52
jlfagundes <jlfagundes@gmail.com>
2021-10-12 18:53:15 -0300

    fix: conneciton bd mongo

--- src/models/connections.js
+++ src/models/connections.js
@@ -1,25 +1,23 @@
 const { MongoClient } = require('mongodb');
 
-const MONGO_DB_URL = 'mongodb://127.0.0.1:27017/cookmaster';
+require('dotenv').config();
 
-let Cookmaster = null;
+// const MONGO_DB_URL = process.env.MONGO_DB_URL || 'mongodb://mongodb:27017/Cookmaster';
 
-async function getConnection() {
-  if (Cookmaster) return Promise.resolve(Cookmaster);
-  return MongoClient.connect(MONGO_DB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-    .then((conn) => conn.db('model_example'))
-    .then((dbCookMaster) => {
-      Cookmaster = dbCookMaster;
-      return Cookmaster;
-    })
-    .catch((err) => {
-      console.error(err);
-    });
-}
+const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster';
 
-module.exports = {
-  getConnection,
-};
+const OPTIONS = { useNewUrlParser: true, useUnifiedTopology: true };
+
+const DB_NAME = 'Cookmaster';
+
+let db = null;
+
+const getConnection = async () => (
+  db ? Promise.resolve(db)
+  : MongoClient.connect(MONGO_DB_URL, OPTIONS)
+  .then((con) => {
+    db = con.db(DB_NAME);
+    return db;
+  }));
+
+module.exports = getConnection; 
