179dfaae366a20e91e1e8e332f869813970d4927
Renan Costa <renancosta21@gmail.com>
2021-10-13 19:43:07 -0300

    evaluator lines

--- src/models/connection.js
+++ src/models/connection.js
@@ -1,16 +1,16 @@
 const { MongoClient } = require('mongodb');
 
 // linhas para o Avaliador
-// const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
-// const DB_NAME = 'Cookmaster';
+const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
+const DB_NAME = 'Cookmaster';
 
 const OPTIONS = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 };
 
-const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster';
-const DB_NAME = 'Cookmaster';
+// const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster';
+// const DB_NAME = 'Cookmaster';
 
 let db = null;
 
