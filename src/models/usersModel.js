5a9ae17268d01dc597010480bb7611f22749da1c
Anderson Silva <andssilva.23@gmail.com>
2021-10-15 20:18:19 -0400

    feat: :rocket: cria o model de users

---
+++ src/models/usersModel.js
@@ -0,0 +1,14 @@
+// const { ObjectID } = require('mongodb');
+const connection = require('./connection');
+
+const createUser = async (name, email, password) => {
+  const db = await connection();
+  const newUser = await db.collection('users').insertOne({ name, email, password, role: 'user' })
+    .then((res) => ({ _id: res.insertedId, name, email }));
+
+  return newUser;
+};
+
+module.exports = {
+  createUser,
+};
