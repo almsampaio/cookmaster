4495d04fb07fe51000295a4f43fb74c423c3f068
William <w.willers@outlook.com>
2021-10-15 22:18:35 -0400

    Teste avaliador para testes req 1, 2 e 3

---
+++ src/integration-tests/requisito1.test.js
@@ -0,0 +1,135 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const sinon = require('sinon');
+
+const server = require('../api/server');
+
+const { MongoClient } = require('mongodb');
+const { getConnection } = require('./mockDatabaseConnection');
+const { expect } = require('chai');
+
+chai.use(chaiHttp);
+
+describe('Requisito 1 - Teste criação de usuário', () => {
+  describe('quando criado com sucesso', () => {
+    describe('a resposta' , () => {
+      const userRequest = {
+        name: 'Carlos Caramelo',
+        email: 'carlao@gmail.com',
+        password: 'caramelo1999'
+      };
+
+      let response;
+
+      before(async () => {
+        const mockConnection = await getConnection();
+        sinon.stub(MongoClient, 'connect').resolves(mockConnection);
+        response = await chai.request(server).post('/users').send(userRequest);
+      });
+
+      after(() => {
+        MongoClient.connect.restore();
+      });
+
+      it('retorna o status 201', () => {
+        expect(response).to.have.status(201);
+      });
+
+      it('é um objeto', () => {
+        expect(response).to.be.an('object');
+      });
+
+      it('o objeto contém uma chave "user"', () => {
+        expect(response.body).to.have.property('user');
+      });
+
+      it('a chave "user" contem as propriedades "name", "email", "role" e "_id"', () => {
+        expect(response.body.user).to.have.all.keys('name', 'email', 'role', '_id');
+      });
+
+      it('a chave "role" deve possuir o valor "user"', () => {
+        expect(response.body.user.role).to.be.equal('user');
+      });
+    });
+  });
+
+  describe('quando o email cadastrado já existe', () => {
+    describe('a resposta', () => {
+      const userRequest = {
+        name: 'Carlos Caramelo',
+        email: 'carlao@gmail.com',
+        password: 'caramelo1999'
+      };
+
+      let response;
+      let mockConnection;
+
+      before(async () => {
+        mockConnection = await getConnection();
+        sinon.stub(MongoClient, 'connect').resolves(mockConnection);
+        await mockConnection.db('Cookmaster').collection('users').insertOne(userRequest);
+        response = await chai.request(server).post('/users').send(userRequest);
+      });
+
+      after(async () => {
+        await mockConnection.db('Cookmaster').collection('users').deleteMany({});
+        MongoClient.connect.restore();
+      });
+
+      it('retorna o status', () => {
+        expect(response).to.have.status(409);
+      });
+
+      it('é um objeto', () => {
+        expect(response.body).to.be.an('object');
+      });
+
+      it('contém a propriedade "message"', () => {
+        expect(response.body).to.have.property('message');       
+      });
+
+      it('"message" contém a mensagem de erro "Email already registered', () => {
+       expect(response.body.message).to.be.equals('Email already registered');
+      });
+    });
+  });
+
+  describe('quando há algum erro no body da request', () => {
+    describe('a resposta', () => {
+      const userRequest = {
+        name: '',
+        email: 'xesquedele.com.br',
+        password: 'xamaaaaa'
+      };
+
+      let response;
+      let mockConnection;
+
+      before(async () => {
+        mockConnection = await getConnection();
+        sinon.stub(MongoClient, 'connect').resolves(mockConnection);
+        response = await chai.request(server).post('/users').send(userRequest);
+      });
+
+      after(async () => {
+        MongoClient.connect.restore();
+      });
+
+      it('retorna o status 400', () => {
+        expect(response).to.have.status(400);
+      });
+
+      it('é um objeto', () => {
+        expect(response.body).to.be.an('object');
+      });
+
+      it('contém a propriedade "message"', () => {
+        expect(response.body).to.have.property('message');       
+      });
+
+      it('"message" contém a mensagem de erro "Invalid entries. Try again."', () => {
+       expect(response.body.message).to.be.equals('Invalid entries. Try again.');
+      });
+    });
+  });
+});
---
+++ src/integration-tests/requisito3.test.js
@@ -0,0 +1,297 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const sinon = require('sinon');
+
+const server = require('../api/app');
+
+const { MongoClient } = require('mongodb');
+const { getConnection } = require('./mockDatabaseConnection');
+
+const { expect } = chai;
+chai.use(chaiHttp);
+
+describe('Requisito 3 - Testa endpoint para cadastro de receitas', () => {
+  describe('quando é cadastrado com sucesso', () => {
+    describe('a resposta' , () => {
+      const user = {
+        name: 'Carlos Caramelo',
+        email: 'carlao@gmail.com',
+        password: 'caramelo1999'
+      };
+
+      const { name: _, ...userLogInfo }  = user;
+
+      const newRecipe = {
+        name: 'Strogonoff de Frango',
+        ingredients: 'Taca-le frango e bacon',
+        preparation: 'Só dale',
+      };
+
+      let login;
+      let response;
+      let mockConnection;
+
+      before(async () =>  {
+        mockConnection = await getConnection();
+        sinon.stub(MongoClient, 'connect').resolves(mockConnection);
+        await mockConnection.db('Cookmaster').collection('users').insertOne(user);
+        login = await chai.request(server).post('/login').send(userLogInfo);
+        const { token } = login.body;
+        response = await chai
+          .request(server).post('/recipes').send(newRecipe).set({ authorization: token });
+      });
+
+      after(async () => {
+        await mockConnection.db('Cookmaster').collection('users').deleteMany({});
+        await mockConnection.db('Cookmaster').collection('recipes').deleteMany({});
+        MongoClient.connect.restore();
+      });
+
+      it('retorna o status 201', () => {
+       expect(response).to.have.status(201);
+      });
+
+      it('retorna um objeto', () => {
+        expect(response).to.be.an('object');       
+      });
+
+      it('contém a chave "recipe"', () => {
+       expect(response.body).to.have.property('recipe')
+      });
+
+      it('"recipe é um objeto', () => {
+       expect(response.body.recipe).to.be.an('object');
+      });
+
+      it('o objeto contém as chaves, "name", "ingredients", "preparation", "userId", "_id"', () => {
+        expect(response.body.recipe).to.have.all
+          .keys('name', 'ingredients', 'preparation', 'userId', '_id');
+      });
+    });
+  });
+
+  describe('há falha no cadastro', () => {
+    describe('quando "name" nao é informado"', () => {
+      describe('a resposta', () => {
+        const user = {
+          name: 'Carlos Caramelo',
+          email: 'carlao@gmail.com',
+          password: 'caramelo1999'
+        };
+
+        const { name: _, ...userLogInfo }  = user;
+
+        const newRecipe = {
+          ingredients: 'Taca-le frango e bacon',
+          preparation: 'Só dale',
+        };
+
+        let login;
+        let response;
+        let mockConnection;
+
+        before(async () =>  {
+          mockConnection = await getConnection();
+          sinon.stub(MongoClient, 'connect').resolves(mockConnection);
+          await mockConnection.db('Cookmaster').collection('users').insertOne(user);
+          login = await chai.request(server).post('/login').send(userLogInfo);
+          const { token } = login.body;
+          response = await chai
+            .request(server).post('/recipes').send(newRecipe).set({ authorization: token });
+        });
+
+        after(async () => {
+          await mockConnection.db('Cookmaster').collection('users').deleteMany({});
+          MongoClient.connect.restore();
+        });
+
+        it('retorna o status 400', () => {
+         expect(response).to.have.status(400);
+        });
+
+        it('retorna um objeto', () => {
+          expect(response).to.be.an('object');         
+        });
+
+        it('objeto contém a chave "message"', () => {
+         expect(response.body).to.have.property('message');
+        });
+
+        it('"message" possui o valor "Invalid entries. Try again.', () => {
+         expect(response.body.message).to.be.equal('Invalid entries. Try again.')
+        });
+      });
+    });
+
+    describe('quando "ingredients" nao é informado"', () => {
+      describe('a resposta', () => {
+        const user = {
+          name: 'Carlos Caramelo',
+          email: 'carlao@gmail.com',
+          password: 'caramelo1999'
+        };
+
+        const { name: _, ...userLogInfo }  = user;
+
+        const newRecipe = {
+          name: 'Xesque',
+          preparation: 'Dele',
+        };
+
+        let login;
+        let response;
+        let mockConnection;
+
+        before(async () =>  {
+          mockConnection = await getConnection();
+          sinon.stub(MongoClient, 'connect').resolves(mockConnection);
+          await mockConnection.db('Cookmaster').collection('users').insertOne(user);
+          login = await chai.request(server).post('/login').send(userLogInfo);
+          const { token } = login.body;
+          response = await chai
+            .request(server).post('/recipes').send(newRecipe).set({ authorization: token });
+        });
+
+        after(async () => {
+          await mockConnection.db('Cookmaster').collection('users').deleteMany({});
+          MongoClient.connect.restore();
+        });
+
+        it('retorna o status 400', () => {
+         expect(response).to.have.status(400);
+        });
+
+        it('retorna um objeto', () => {
+          expect(response).to.be.an('object');         
+        });
+
+        it('objeto contém a chave "message"', () => {
+         expect(response.body).to.have.property('message');
+        });
+
+        it('"message" possui o valor "Invalid entries. Try again.', () => {
+         expect(response.body.message).to.be.equal('Invalid entries. Try again.')
+        });
+      });
+    });
+
+    describe('quando "preparation" nao é informado"', () => {
+      describe('a resposta', () => {
+        const user = {
+          name: 'Carlos Caramelo',
+          email: 'carlao@gmail.com',
+          password: 'caramelo1999'
+        };
+
+        const { name: _, ...userLogInfo }  = user;
+
+        const newRecipe = {
+          name: 'Chama',
+          ingredients: 'Jimmy',
+        };
+
+        let login;
+        let response;
+        let mockConnection;
+
+        before(async () =>  {
+          mockConnection = await getConnection();
+          sinon.stub(MongoClient, 'connect').resolves(mockConnection);
+          await mockConnection.db('Cookmaster').collection('users').insertOne(user);
+          login = await chai.request(server).post('/login').send(userLogInfo);
+          const { token } = login.body;
+          response = await chai
+            .request(server).post('/recipes').send(newRecipe).set({ authorization: token });
+        });
+
+        after(async () => {
+          await mockConnection.db('Cookmaster').collection('users').deleteMany({});
+          MongoClient.connect.restore();
+        });
+
+        it('retorna o status 400', () => {
+         expect(response).to.have.status(400);
+        });
+
+        it('retorna um objeto', () => {
+          expect(response).to.be.an('object');         
+        });
+
+        it('objeto contém a chave "message"', () => {
+         expect(response.body).to.have.property('message');
+        });
+
+        it('"message" possui o valor "Invalid entries. Try again.', () => {
+         expect(response.body.message).to.be.equal('Invalid entries. Try again.')
+        });
+      });
+    });
+
+    describe('quando o "token" não é válido', () => {
+      describe('a resposta', () => {
+        let response;
+        let mockConnection;
+
+        before(async () =>  {
+          mockConnection = await getConnection();
+          sinon.stub(MongoClient, 'connect').resolves(mockConnection);
+          response = await chai
+            .request(server).post('/recipes').send({}).set({ authorization: 'invalidToken' });
+        });
+
+        after(async () => {
+          await mockConnection.db('Cookmaster').collection('users').deleteMany({});
+          MongoClient.connect.restore();
+        });
+        it('retorna o status 401', () => {
+          expect(response).to.have.status(401);
+        });
+
+        it('retorna um objeto', () => {
+          expect(response.body).to.be.an('object');
+        });
+
+        it('objeto contém a chave "message"', () => {
+          expect(response.body).to.have.property('message')
+        });
+
+        it('"message" possui o valor "jwt malformed', () => {
+          expect(response.body.message).to.be.equal('jwt malformed');
+        });
+      });
+    });
+
+    describe('quando o "token" não é informado', () => {
+      describe('a resposta', () => {
+        let response;
+        let mockConnection;
+
+        before(async () =>  {
+          mockConnection = await getConnection();
+          sinon.stub(MongoClient, 'connect').resolves(mockConnection);
+          response = await chai
+            .request(server).post('/recipes').send({});
+        });
+
+        after(async () => {
+          MongoClient.connect.restore();
+        });
+        it('retorna o status 401', () => {
+          expect(response).to.have.status(401);
+        });
+
+        it('retorna um objeto', () => {
+          expect(response.body).to.be.an('object');
+        });
+
+        it('objeto contém a chave "message"', () => {
+          expect(response.body).to.have.property('message')
+        });
+
+        it('"message" possui o valor "missing auth token"', () => {
+          expect(response.body.message).to.be.equal('missing auth token');
+        });
+      });
+    });
+  })
+});
