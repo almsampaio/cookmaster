1cafa1c539a378ce6423ec5616cf967ec3a64ea6
Ederson S <edersonrodrigo31@gmail.com>
2021-10-17 12:11:22 -0300

    try fix

--- src/api/app.js
+++ src/api/app.js
@@ -4,14 +4,13 @@ const globalErrorHandler = require('./middlewares/globalErrorHandler');
 
 const app = express();
 app.use(express.json());
+app.use(routes);
+app.use(globalErrorHandler);
 
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
   response.send();
 });
 // Não remover esse end-point, ele é necessário para o avaliador
-
-app.use(routes);
-app.use(globalErrorHandler);
 
 module.exports = app;
