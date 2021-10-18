b1e3c77bb2a26378bfab4f89822107240009c10d
jlfagundes <jlfagundes@gmail.com>
2021-10-18 10:24:14 -0300

    feat: challenge 4 - create model GET all recipes

--- src/models/recipes.js
+++ src/models/recipes.js
@@ -8,6 +8,14 @@ const create = async (name, ingredients, preparations, userId) => {
   return newRecipe;
 };
 
+const getAll = async () => {
+  const connectDb = await getConnection();
+  const allRecipes = await connectDb.collection('recipes')
+    .find([]).toArray();
+  return allRecipes;
+};
+
 module.exports = {
   create,
+  getAll,
 };
