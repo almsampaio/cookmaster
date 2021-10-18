5d42141d65bfcccf9fe90117e0e71d16f6e4d800
jlfagundes <jlfagundes@gmail.com>
2021-10-18 15:54:32 -0300

    fix: challenge 3 - model create recipe

--- src/models/recipes.js
+++ src/models/recipes.js
@@ -1,10 +1,10 @@
 const { ObjectId } = require('mongodb');
 const getConnection = require('./connections');
 
-const create = async (name, ingredients, preparations, userId) => {
+const create = async (name, ingredients, preparation, userId) => {
   const connectDb = await getConnection();
   const { ops: newRecipe } = await connectDb.collection('recipes')
-    .insertOne({ name, ingredients, preparations, userId });
+    .insertOne({ name, ingredients, preparation, userId });
 
   return newRecipe;
 };
--- src/services/login.js
+++ src/services/login.js
@@ -4,25 +4,6 @@ const validations = require('../schemas/validationsLogin');
 
 const SECRET = 'mysecretultrasecret';
 
-// const loginUser = async (email, password) => {
-//   const validateEmail = await validations.validateEmail(email);
-//   if (validateEmail) return { status: 401, data: validateEmail };
-
-//   const validatePassword = await validations.validatePassword(password);
-//   if (validatePassword) return { status: 401, data: validatePassword };
-
-//   const verifyEmail = await validations.verifyEmail(email);
-//   if (verifyEmail) return { status: 401, data: verifyEmail };
-
-//   const searchUser = await modelUsers.getAll();
-//   const { password: _, ...userPayload } = searchUser;
-//   const token = jwt.sign(
-//     userPayload,
-//     SECRET,
-//   );
-//   return { status: 200, data: { token } };
-// };
-
 const loginUser = async (email, password) => {
   const userSearch = await modelLogin.searchEmailUser(email);
 
