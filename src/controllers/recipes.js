a41bde80196d3e4ff94941e2bbc513cc1f1baf84
jlfagundes <jlfagundes@gmail.com>
2021-10-18 10:31:17 -0300

    feat: challenge 4 - controller GET recipes

--- src/controllers/recipes.js
+++ src/controllers/recipes.js
@@ -7,6 +7,12 @@ const create = async (req, res) => {
   return res.status(status).json(data);
 };
 
+const getAll = async (_req, res) => {
+  const { status, data } = await serviceRecipes.getAll();
+  return res.status(status).json(data);
+};
+
 module.exports = {
   create,
+  getAll,
 };
