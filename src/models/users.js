5e6aacb11ad4f7cd607dca4b185faaaa23112866
jlfagundes <jlfagundes@gmail.com>
2021-10-19 23:08:15 -0300

    feat: challenge 12 - model new admin

--- src/models/users.js
+++ src/models/users.js
@@ -19,7 +19,20 @@ const getAll = async () => {
   return allUsers;
 };
 
+const createAdmin = async (name, email, password) => {
+  const connectBd = await getConnection();
+  const { ops: newUser } = await connectBd.collection('users')
+    .insertOne({
+      name,
+      email,
+      password,
+      role: 'admin',
+    });
+  return newUser[0];
+};
+
 module.exports = {
   create,
   getAll,
+  createAdmin,
 };
